64ccb167f3c2fc03f6a488a4a69c31011f03effc
Melissa Miller <lis.d.miller@gmail.com>
2016-04-09 18:32:33 -0400

    Sets up schema for restaurants and menu items

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,20 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema;
+  ObjectId = Schema.ObjectId;
+
+var MenuSchema = new Schema({
+  title: String
+}); // end MenuSchema
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  menu: [MenuSchema]
+}); // end RestaurantSchema
