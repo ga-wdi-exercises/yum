1d3ff32bfe095dde3ae6f95900cd4ac1bd5dfa34
YOUR FULL NAME <farishta.haider@gmail.com>
2016-04-09 15:29:24 -0400

    Added model and schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,22 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function(){
+  console.log("database has been connected!");
+});
+
+var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId
+
+var RestaurantSchema = new Schema({
+  name: String
+});
+
+var MenuSchema = new Schema({
+  name: String,
+  restaurant: [RestaurantSchema]
+});
+
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+var MenuModel = mongoose.model("Menu", MenuSchema);
