a4b3adad41a059947430f74fbf771381d1d4b1c3
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-04-10 14:55:45 -0400

    can add restaurant and menu item

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,39 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+
+  var Schema = mongoose.Schema,
+  ObjectId = Schema.ObejectId;
+
+  var ItemSchema = new Schema({
+    title: String
+  });
+
+  var RestaurantSchema = new Schema({
+    name: String,
+    address: {street: String, zipcode: Number},
+    yelp_url: String,
+    items: [ItemSchema]
+  });
+
+  var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+  var ItemModel = mongoose.model("Item", ItemSchema);
+
+  var diner = new RestaurantModel({name: "The Diner", address:{street: "101 18th Street", zipcode: 20006}, yelp_url:"www.yelp.com"});
+
+  var chicken = new ItemModel({title: "chicken"});
+
+  diner.items.push(chicken);
+  diner.save(function(err, restaurant){
+    if (err){
+      console.log(err);
+    }
+    else{
+      console.log(restaurant);
+    }
+  });
+
+
+});
