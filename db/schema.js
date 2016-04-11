613df9fc41d745f2a56e418f251e70f8ece2bf9d
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-04-11 13:41:37 -0400

    restaurant and menu model and schema created

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,21 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.schema;
+
+var RestaurantSchema = new Schema({
+
+})
+
+var MenuSchema = new Schema({
+
+})
+
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+var MenuModel = mongoose.model("Menu", MenuSchema);
+
+module.exports = {
+  RestaurantModel: RestaurantModel,
+  MenuModel: MenuModel
+}
