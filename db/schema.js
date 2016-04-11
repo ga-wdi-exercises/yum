06130f6d6b41825a3e269ac2b3ebf24c227ad61d
Hilary Masland <hilarymasland@gmail.com>
2016-04-11 13:37:27 -0400

    initial commit - schema data

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,18 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+
+ var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId
+
+  var RestaurantSchema = new Schema({
+    name: String,
+    address:
+    yelp_url: String,
+    items: []
+  })
+
+  var MenuSchema = new Schema({
+    title: String,
+  })
