21bf9d6cbd451edb2465d97ef32669a76201b556
Christine Movius <christine.movius@gmail.com>
2016-04-11 13:50:16 -0400

    Initial Commit. MongoDB connected to repo via mongoose.

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log("Yum db connected!");
+});
