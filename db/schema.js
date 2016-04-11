9a61fd9179b924c115bb21bb0a481f3ef4fef4c0
Christine Movius <christine.movius@gmail.com>
2016-04-11 14:11:55 -0400

    Define restaurant and item schema and models.

--- db/schema.js
+++ db/schema.js
@@ -6,3 +6,28 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
   console.log("Yum db connected!");
 });
+
+var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId
+
+var ItemSchema = new Schema({
+  title: String
+});
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelp: String,
+  items: [ItemSchema]
+});
+
+var ItemModel = mongoose.model("Item", ItemSchema);
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+
+module.exports = {
+  ItemModel: ItemModel,
+  RestaurantModel: RestaurantModel
+};
