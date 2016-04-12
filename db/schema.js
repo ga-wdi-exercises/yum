252fdf963513bc80d86155659f5ea7ed6ac8f686
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-04-11 20:56:56 -0400

    getting lots of errors and cannot seem to fix depsite looking at solution will get help tomorrow

--- db/schema.js
+++ db/schema.js
@@ -3,18 +3,18 @@ mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-var Schema = mongoose.schema;
+var Schema = mongoose.Schema;
 
 var RestaurantSchema = new Schema({
-  name: String
+  name: String,
   address: {street: String, zipcode: Number},
-  yelp_url: String
-  menu: [MenuSchema]
-})
+  yelp_url: String,
+  items: [MenuSchema]
+});
 
 var MenuSchema = new Schema({
   title: String
-})
+});
 
 var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
 var MenuModel = mongoose.model("Menu", MenuSchema);
