da86a9abbb46d936adffb39d4da55360ef6d62ed
Maureen VOGEL <maureenvogel@MaureenOGELsMBP.home>
2016-04-11 20:57:25 -0400

    Created a restaurant

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,37 @@
 var Schema = require("../db/schema.js");
+
+
+var MenuItem = Schema.MenuItem;
+var Restaurant = Schema.Restaurant;
+
+Restaurant.remove({}, function(err){
+  console.log(err)
+});
+
+MenuItem.remove({}, function(err){
+  console.log(err)
+})
+
+var inAndOut= new Restaurant(
+  {
+    name: "In and Out",
+    address: {street: "M street", zipcode: 22310, state: "CA"},
+    yelp: "inandout.com"
+  });
+
+var hamburger = new MenuItem({title: "hamburger"});
+var fries = new MenuItem({title: "fries"});
+
+var restaurants = [inAndOut];
+var menuitems = [hamburger, fries];
+
+for(var i = 0; i< restaurants.length; i++){
+  restaurants[i].menuitems.push(menuitem[i], menuitem[i+1])
+  restaurants[i].save(function(err){
+    if(err){
+      console.log(err)
+    } else {
+      console.log("restaurant was saved");
+    }
+  })
+}
--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+
+var Restaurant = Schema.Restaurant;
+var MenuItem = Schema.MenuItem;
+
+// create a restaurant
+Restaurant.create({name: "McDonalads", "address.zipcode": 22222, "address.state": "FL"}, function(err, restaurant){
+  if(err)return handleError(err);
+  console.log(restaurant.name + " was created");
+});
+
+// find by name
