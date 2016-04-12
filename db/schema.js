04720fb3de0eae02c2c31da526c8fea58716e368
Caitlin Daitch <caitlin.daitch@gmail.com>
2016-04-12 17:37:14 -0400

    Delete working

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,33 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/yum");
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", function(){
+  console.log("Connected!");
+})
+
+var Schema = mongoose.Schema,
+  ObjectID = Schema.ObjectID
+
+var ItemsSchema = new Schema({
+  title: String
+})
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [ItemsSchema]
+})
+
+var ItemsModel = mongoose.model("Item", ItemsSchema);
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+
+module.exports = {
+  ItemsModel: ItemsModel,
+  RestaurantModel: RestaurantModel
+}
