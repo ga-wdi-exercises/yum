db4267833d7a3bf284bbe33fe51f65b86fc4db90
Charles <charlesthwang@gmail.com>
2016-04-12 13:08:55 -0400

    added restaurantsController.js and defined index method to find all restaurants

---
+++ controllers/restaurantsController.js
@@ -0,0 +1,17 @@
+var Schema = require("../db/schema.js");
+var RestaurantModel = Schema.RestaurantModel
+var ItemModel = Schema.ItemModel
+
+var restaurantsController = {
+  index: function(){
+    RestaurantModel.find({}, function(err, docs){
+      if (err){
+        console.log(err)
+      }else{
+        console.log(docs)
+      }
+    });
+  }
+}
+
+restaurantsController.index();
