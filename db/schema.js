131808436d6c4e5f15dd5c0d7f1b4e86165d28a2
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-04-12 14:25:31 -0400

    able to seed the db

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,26 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema,
+ ObjectId = Schema.ObjectId
+
+var MenuSchema = new Schema({
+  title: String
+});
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: { street: String, state: String, zipcode: Number },
+  yelp: String,
+  items: [MenuSchema]
+});
+
+
+var MenuModel = mongoose.model("Menu", MenuSchema);
+var RestaurantModel = mongoose.model('Restaurant', RestaurantSchema);
+
+module.exports = {
+  RestaurantModel: RestaurantModel,
+  MenuModel: MenuModel
+};
