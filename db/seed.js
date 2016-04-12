ef2241a79af73b69145d202dc3eb697ca508a9ef
Bipin Neupane <icomp2@hotmail.com>
2016-04-12 19:25:48 -0400

    created schema

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,28 @@
+var mongoose = require('mongoose');
 var Schema = require("../db/schema.js");
+
+var Restaurant = Schema.RestaurantModel;
+var MenuItem = Schema.MenuItemModel;
+
+RestaurantModel.remove({}, function(err){
+  console.log(err)
+});
+MenuItemModel.remove({}, function(err){
+  console.log(err)
+});
+
+var rest = new StudentModel({
+  name: "Joe's Pizza",
+  address: "12 main st",
+  zipcode: 20111,
+  yelpUrl: "https://www.yelp.com/joes"
+});
+
+var menu = new MenuItemModel({name: "Fried pizza"});
+
+var project1 = new ProjectModel({title: "project1", unit: "JS"})
+
+var students = [anna, brandon]
+var projects [project1]
+
+anna.save
