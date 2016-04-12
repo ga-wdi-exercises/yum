7238048b9ed3c2627bb42429cc579aab44e27f1a
Bipin Neupane <icomp2@hotmail.com>
2016-04-12 19:58:58 -0400

    adding to menu

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ var Schema = require("./db/Schema.js");
 var Restaurant = Schema.RestaurantModel;
 var MenuItem = Schema.MenuItemModel;
 
-function 
+function Restaurant(restaurant, street, zip, yelp){
+  restaurant.create({
+    name: restaurant,
+    address: street,
+    zipcode: zipcode,
+    yelpUrl: linkYelp
+  });
+})
+
+function addToMenu(restaurant, food){
+  Restaurant.findOne({name: restaurant}, function(err, docs){
+    docs.menuItem.push(new MenuItem({title: food}));
+    docs.save(function(err, results){
+      if(err){
+        console.log(error);
+      }
+      else {
+        console.log(result);
+      }
+
+    });
+  });
