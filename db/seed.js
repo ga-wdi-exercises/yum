78800f5f6c7ed7a7a7d5ac3c1bef41e3f2db1854
Brendan Magee <bmagee@gmail.com>
2016-04-13 17:46:37 -0400

    added restaurants and items to seed data

--- db/seed.js
+++ db/seed.js
@@ -11,3 +11,28 @@ RestaurantModel.remove({}, function(err){
 ItemModel.remove({}, function(err){
   console.log(err);
 });
+
+var sandovan = new RestaurantModel({
+  name: "Sandovan's Restaurant and Lounge",
+  address : {
+    street : "4809 Georgia Avenue NW",
+    zipcode : 20010,
+  },
+  yelpUrl: "http://www.yelp.com/biz/sandovans-restaurant-and-lounge-washington",
+  items: []
+});
+
+var johnnyCarryout = new RestaurantModel({
+  name: "Johnny's Carryout",
+  address : {
+    street : "3445 14th Street NW",
+    zipcode : 20010,
+  },
+  yelpUrl: "http://www.yelp.com/biz/johnnys-carryout-washington",
+  items: []
+});
+
+var cheesesteak = new ItemModel({title: "Cheesesteak"});
+var jerkChicken = new ItemModel({title: "Jerk Chicken"});
+var plantains = new ItemModel({title: "Fried Plantains"});
+var wings = new ItemModel({title: "Wings with Mumbo Sauce"});
