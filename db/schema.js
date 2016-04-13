37fc36b04b69e5748a14f7eb68e0af93ee8889c3
Hilary Masland <hilarymasland@gmail.com>
2016-04-13 08:36:52 -0400

    moved info back - first was correct

--- db/schema.js
+++ db/schema.js
@@ -3,18 +3,17 @@ mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
+var Schema = mongoose.Schema,
+ ObjectId = Schema.ObjectId
 
- var Schema = mongoose.Schema,
-  ObjectId = Schema.ObjectId
-
-  var RestaurantSchema = new Schema({
-    name: String,
-    address: {"street": String, "zipcode_property": Number},
-    yelp_url: String,
-    items: []
-  })
+ var RestaurantSchema = new Schema({
+   name: String,
+   address: {"street": String, "zipcode_property": Number},
+   yelp_url: String,
+   items: []
+ })
 
 
-  var MenuSchema = new Schema({
-    title: String
-  })
+ var MenuSchema = new Schema({
+   title: String
+ })
