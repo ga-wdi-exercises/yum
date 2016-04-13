370e136cea3cb85133871001800e7c3305c16d72
Cam <camlatimer@gmail.com>
2016-04-13 14:23:00 -0400

    starts filling out seed data

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,22 @@
-var Schema = require("../db/schema.js");
+var mongoose = require("mongoose"),
+    Schema = require("../db/schema.js"),
+    RestaurantModel = Schema.RestaurantModel,
+    MenuItemModel = Schema.MenuItemModel;
+
+RestaurantModel.remove({}, function(err){
+  console.log(err);
+});
+MenuItemModel.remove({}, function(err){
+  console.log(err);
+});
+
+var maketto = new RestaurantModel({name: 'Maketto'}),
+    toki = new RestaurantModel({name: 'Toki'}),
+    impala = new RestaurantModel({name: 'Impala'}),
+    restaurantsRow = [maketto, toki, impala];
+
+restaurantsRow.forEach(function(restaurant){
+  restaurant.address = 111 + ' ' + restaurant.name + ' Street';
+  restaurant.yelp = 'http://yelp/' + restaurant.name;
+  
+})
