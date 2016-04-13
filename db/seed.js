cd2cc748a7bf2d4904ca3b003f24bbb8ed1b1008
Max Kaye <maximillianchina@gmail.com>
2016-04-13 14:27:58 -0400

    added menu items to MenuModel

--- db/seed.js
+++ db/seed.js
@@ -21,3 +21,37 @@ var momofuku = new RestaurantModel({
   },
   yelpurl: "http://www.yelp.com/biz/momofuku-ccdc-washington"
 });
+
+var classic = new MenuModel({
+  title: "Buttermilk Chicken and Pickles Sandwich"
+});
+var bbq = new MenuModel({
+  title: "Buttermilk Chicken and Pepperjack Sandwich"
+});
+var southern = new MenuModel({
+  title: "Buttermilk Chicken, Bacon, and Slaw Sandwich"
+});
+var egg = new MenuModel({
+  title: "Buttermilk Chicken, Bacon, Eggs, and Grits Sandwich"
+});
+
+
+var crack = new MenuModel({
+  title: "Crack Pie"
+});
+var milk = new MenuModel({
+  title: "Cereal Milk"
+});
+var truffles = new MenuModel({
+  title: "Cake Truffles"
+});
+var granola = new MenuModel({
+  title: "quinoi granola with berries and yogurt"
+});
+
+
+jacobspickles.menu.push(classic, bbq, southern, egg);
+momofuku.menu.push(crack, milk, truffles, granola);
+
+
+var restaurants = [jacobspickles, momofuku];
