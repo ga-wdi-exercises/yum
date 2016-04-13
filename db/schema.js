5ebe6b386b2bf074bfccf931b6f9e4d7a19ff923
Brendan Magee <bmagee@gmail.com>
2016-04-13 14:13:19 -0400

    added items and restaurant schemata

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,20 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema,
+ObjectId = Schema.ObjectId;
+
+var ItemsSchema = new Schema({
+  title: String
+});
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [ ItemsSchema ]
+});
