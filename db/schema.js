a317df69713b8b1f757f477ce29f72190f73f750
Cam <camlatimer@gmail.com>
2016-04-13 14:03:20 -0400

    adds schemas and models for restaurants and menu items

--- db/schema.js
+++ db/schema.js
@@ -2,7 +2,24 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: {type: Number}
+  },
+  yelp: String,
+  items: [MenuItemSchema]
+})
+var MenuItemSchema = new Schema({
+  title: String
+})
+
+var RestaurantModel = mongoose.model('Restaurant', RestaurantSchema);
+var MenuItemModel = mongoose.model('MenuItem', MenuItemSchema)
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
