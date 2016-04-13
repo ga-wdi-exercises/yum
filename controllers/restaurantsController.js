7ce7c950fd9ffa30e88f161977f4f3af2983833c
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-04-13 17:54:14 -0400

    completes through level 7

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -20,13 +20,47 @@ var restaurantsController = {
         console.log(restaurant);
       }
     })
+  },
+  showZip: function(req){
+    RestaurantModel.find({"address.zipcode": req.zipcode}, function(err, restaurant){
+      if(err){
+        console.log(err);
+      }else{
+        console.log(restaurant);
+      }
+    })
+  },
+  updateName: function(req, update){
+    RestaurantModel.findOneAndUpdate({name: req.name}, {name: update.name}, {new: true}, function(err, restaurant){
+      if(err){
+        console.log(err);
+      }else{
+        console.log(restaurant);
+      }
+    });
+  },
+  destroy: function(req){
+    RestaurantModel.findOneAndRemove(req, function(err, restaurant){
+      if(err){
+        console.log(err);
+      }else{
+        console.log(restaurant);
+      }
+    });
   }
 }
 
 //index view
-restaurantsController.index({});
+// restaurantsController.index({});
 
 //find one by name
-restaurantsController.show({name: "Thai Palace"});
+// restaurantsController.show({name: "Thai Palace"});
 
-//
+//finds all based on zip
+// restaurantsController.showZip({zipcode: 20815});
+
+//updates restaurant name
+// restaurantsController.updateName({name: "Thai Palace"}, {name: "New Thai House"})
+
+//removes restaurant
+restaurantsController.destroy({name: "New Thai House"})
