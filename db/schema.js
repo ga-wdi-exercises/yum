5eeb54d18d1f21bd508e14fbfb13ba9a4c97db29
BrittanyBranson <brittanydibble@Brittanys-MacBook-Pro.local>
2016-04-13 20:53:09 -0400

    added restaurant schema data as well as yelp

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,32 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once("open", function(){
+  console.log("Connected!");
+})
+
+var Schema = mongoose.Schema,
+ObjectID = Schema.ObjectID
+
+var ItemsSchema = new Schema({
+  title: String
+})
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [ItemsSchema]
+})
+
+var ItemsModel = mongoose.model("Item", ItemsSchema);
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+
+module.exports = {
+  ItemsModel: ItemsModel,
+  RestaurantModel: RestaurantModel
+}
