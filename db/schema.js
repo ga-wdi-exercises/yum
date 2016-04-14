9d38718c8a93938543a262ec4a6c06c1a85875a8
caseyagil <caseyagil@gmail.com>
2016-04-13 21:40:39 -0400

    seed data and schema done

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,25 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+ mongoose.connect('mongodb://localhost/yum');
+ var db = mongoose.connection;
+ db.on('error', console.error.bind(console, 'connection error:'));
+
+ var Schema = mongoose.schema;
+
+  var RestaurantSchema = new Schema({
+    name: String
+   address: [{"street": String}, {"zipcode": Number}]
+    yelp_url: String
+   menu: [Schema]
+  })
+
+  var MenuSchema = new Schema({
+   title: String
+ })
+
+ var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+ var MenuModel = mongoose.model("Menu", MenuSchema);
+
+ module.exports = {
+   RestaurantModel: RestaurantModel,
+   MenuModel: MenuModel
+ }
