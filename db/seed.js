56fde75fe3773290405ef95b2fafef9e68a7616d
Cam <camlatimer@gmail.com>
2016-04-14 08:53:26 -0400

    changed model names

--- db/seed.js
+++ db/seed.js
@@ -1,25 +1,25 @@
 var mongoose = require("mongoose"),
     Schema = require("../db/schema.js"),
-    RestaurantModel = Schema.RestaurantModel,
-    MenuItemModel = Schema.MenuItemModel;
+    Restaurant = Schema.RestaurantModel,
+    MenuItem = Schema.MenuItemModel;
 
-RestaurantModel.remove({}, function(err){
+Restaurant.remove({}, function(err){
   console.log(err);
 });
-MenuItemModel.remove({}, function(err){
+MenuItem.remove({}, function(err){
   console.log(err);
 });
 
-var maketto = new RestaurantModel({name: 'Maketto'}),
-    toki = new RestaurantModel({name: 'Toki'}),
-    impala = new RestaurantModel({name: 'Impala'}),
+var maketto = new Restaurant({name: 'Maketto'}),
+    toki = new Restaurant({name: 'Toki'}),
+    impala = new Restaurant({name: 'Impala'}),
     restaurantsRow = [maketto, toki, impala];
 
 restaurantsRow.forEach(function(restaurant){
   restaurant.address.street = 111 + ' ' + restaurant.name + ' Street';
   restaurant.address.zipcode = 20009;
   restaurant.yelp = 'http://yelp/' + restaurant.name;
-  var dish = new MenuItemModel({title: restaurant.name + ' dish'})
+  var dish = new MenuItem({title: restaurant.name + ' dish'})
   restaurant.items.push(dish)
   restaurant.save();
   console.log(restaurant);
