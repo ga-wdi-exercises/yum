09b2cacc1ce8638b707e9326c5a6cd278388355f
Crucnhwrap78 <Hayesapples78@gmail.com>
2016-04-14 06:14:38 -0400

    added basic CRUD for restaurant

--- db/seed.js
+++ db/seed.js
@@ -13,11 +13,11 @@ MenuItem.remove({}, function(err){
   console.log(err)
 })
 
+//creates new objects of resturants to put in database
+  var resturant1 = new Restaurant({ name: "Taco Bell", address: { street: "40 Massachusetts Ave NE", zipcode: 20002, }, yelp: "http://www.yelp.com/biz/taco-bell-washington-16"})
 
-  resturant1 = new Restaurant({ name: "Taco Bell", address: { street: "40 Massachusetts Ave NE", zipcode: 20002, }, yelp: "http://www.yelp.com/biz/taco-bell-washington-16"})
 
-
-
+//creates new objects of menu to put in database
   var food1 = new Menu({title: "crunchwrap supreme"})
 
   var menu = [food1]
