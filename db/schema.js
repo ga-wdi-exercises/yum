7141482a163a3340e0c284cfd07852dc50f7f4af
Crucnhwrap78 <Hayesapples78@gmail.com>
2016-04-14 05:42:29 -0400

    created models for restaurant db

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,35 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("database is now connected");
+});
+
+//used to create schemas
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId
+
+
+var RestaurantSchema = mongoose.Schema({
+  name: String,
+  address: {
+    street: String,
+    state: String,
+    zipcode: Number
+  },
+  yelp: String,
+  //reference to MenuItem
+  menu: [{type: Schemas.ObjectId, ref: "Menu"}
+});
+
+var MenuSchema = mongoose.Schema({
+  name: String
+});
+
+var MenuModel = mongoose.model("Menu", MenuItemSchema);
+var RestaurantModel = mongoose.model('Restaurant', RestaurantSchema);
+
+module.exports = {
+  Restaurant: RestaurantModel,
+  Menu: MenuModel
+};
