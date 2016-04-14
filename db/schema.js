0b5a5b39cfb18ee98e10ae523d2f3e76c24bda13
Ryan Krolick <rkrolick@gmail.com>
2016-04-13 23:44:24 -0400

    schema and seeds created.

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,23 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema;
+
+var MenuItemSchema = new Schema({
+  title: String
+});
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {street: String, zip: Number},
+  yelp: String,
+  menuItems: [MenuItemSchema]
+});
+
+var MenuItemModel = mongoose.model("MenuItem", MenuItemSchema);
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+
+module.exports = {
+  MenuItemModel: MenuItemModel,
+  RestaurantModel: RestaurantModel
+}
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,46 @@
 var Schema = require("../db/schema.js");
+
+var RestaurantModel = Schema.RestaurantModel;
+var MenuItemModel = Schema.MenuItemModel;
+
+RestaurantModel.remove({});
+MenuItemModel.remove({});
+
+var albertos = new RestaurantModel({
+  name: "Alberto's",
+  address: {street: "Dupont Circle", zip: 20037},
+  yelp: "www.yelp.com/albertos",
+  menuItems: []
+})
+var deepDish = new MenuItemModel({title:"Deep Dish Pizza"});
+var nyStyle = new MenuItemModel({title:"New York Style Pizza"});
+albertos.menuItems.push(deepDish);
+albertos.menuItems.push(nyStyle);
+albertos.save();
+deepDish.save();
+nyStyle.save();
+
+var taylors = new RestaurantModel({
+  name: "Taylor's",
+  address: {street: "M St. NW", zip: 20037},
+  yelp: "www.yelp.com/taylors",
+  menuItems: []
+});
+var cherrySt = new MenuItemModel({title: "Cherry St. Sub"});
+var riceBalls = new MenuItemModel({title: "Rice Ball Thingies"});
+taylors.menuItems.push(cherrySt);
+taylors.menuItems.push(riceBalls);
+taylors.save();
+cherrySt.save();
+riceBalls.save();
+
+var thatPlaceDownstairs = new RestaurantModel({
+  name: "That Place Downstairs",
+  address: {street: "Downstairs", zip:18224},
+  yelp: "www.yelp.com/that_place_downstairs",
+  menuItems: []
+});
+var breakfast = new MenuItemModel({title: "Sausage & Cheese"});
+thatPlaceDownstairs.menuItems.push(breakfast);
+thatPlaceDownstairs.save();
+breakfast.save();
