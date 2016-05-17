03f4e6510d112a8c54b9700dfd1b9224f4bf2350
Spicy Legato <jpqreichard@gmail.com>
2016-05-17 13:42:01 -0400

    adds express

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,19 @@
 var Schema = require("../db/schema.js");
+
+var Restaurants = {
+  name: "Bob Pizza's Artisinal Apartment Sandwiches",
+  address: {
+    street: "666 Condos Ave",
+    zip: 90210
+  },
+  yelp_url: "http://www.yelp.com/Bob_Pizza",
+  items: bobPizzaItems
+};
+
+var bobPizzaItems = [
+  {title: "PizzaBurgs"},
+  {title: "Servo Motor Parts"},
+  {title: "PizzaBurgs"}
+]
+
+//module.export something?
