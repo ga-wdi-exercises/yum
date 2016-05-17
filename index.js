82064ffc107fa3b32888360c98edae032bfdd631
Spicy Legato <jpqreichard@gmail.com>
2016-05-17 13:34:12 -0400

    adds express lin and hbs lib and view engine

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-var mongoose = require('mongoose');
-var Schema = require("./db/schema.js");
+var express   = require('express');
+var hbs   = require('express-handlebars');
+var mongoose  = require('mongoose');
+var Schema    = require("./db/schema.js");
+
+var app = express();
+
+app.set("port", process.env.PORT || 3001);
+app.set("view engine", "hbs");
+app.engine(".hbs",hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
