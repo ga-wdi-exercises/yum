3f50b1725f2338e94576d9360f9259221852ad7d
Spicy Legato <jpqreichard@gmail.com>
2016-05-17 14:02:50 -0400

    test string

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var express   = require('express');
-var hbs   = require('express-handlebars');
+var hbs       = require('express-handlebars');
 var mongoose  = require('mongoose');
-var Schema    = require("./db/schema.js");
+var Schema    = require('./db/schema.js');
 
 var app = express();
 
 app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs");
-app.engine(".hbs",hbs({
+app.engine(".hbs", hbs({
   extname: ".hbs",
   partialsDir: "views/",
   layoutsDir: "views/",
   defaultLayout: "layout-main"
 }));
+
+app.get("/", function(req, res){
+  res.send("Yeahhhhh");
+});
+
+app.listen(app.get("port"), function(){
+  console.log(app.get("port") + " running");
+});
