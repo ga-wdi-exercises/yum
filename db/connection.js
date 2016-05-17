92fdfef2ef80ab4ac15c71c0c1e919e03b1e7e48
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-17 17:00:05 -0400

    added crud

---
+++ db/connection.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+var MenuItemSchema = new mongoose.Schema(
+  {
+    title: String
+  }
+);
+
+var RestaurantSchema = new mongoose.Schema(
+  {
+    name: String,
+    address: {street: String, zip: Number},
+    yelpUrl: String,
+    items: Array
+  }
+);
+
+mongoose.model('Restaurant', RestaurantSchema);
+mongoose.model('MenuItem', MenuItemSchema);
+
+mongoose.connect('mongodb://localhost/yum');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){console.log("connected!")})
+
+module.exports = mongoose;
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,14 @@
-var Schema = require("../db/schema.js");
+var mongoose = require("./connection");
+var seedData = require("./seeds");
+
+var Restaurant = mongoose.model("Restaurant");
+
+Restaurant.remove({}).then(function(err){
+  Restaurant.collection.insert(seedData[0])
+});
+
+var MenuItem = mongoose.model("MenuItem");
+
+MenuItem.remove({}, function(err){
+  MenuItem.collection.insert(seedData[1])
+});
