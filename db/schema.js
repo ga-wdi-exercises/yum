ce082f3ed9ccd66afadc3077859843fca9691cba
Louis <LOHankins@gmail.com>
2016-05-18 18:45:51 -0400

    first attempt at schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,24 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var RestaurantSchema = new mongoose.Schema({
+	name: String,
+	address = {
+		line1: String,
+		line2: String,
+		city : String,
+		state: String,
+		zip  : Number
+	},
+	yelp_url : String,
+	MenuSchema = new mongoose.Schema({
+		title: String,
+		item : Array 
+	}),
+});
+
+mongoose.model("Restaurant", RestaurantSchema);
+
+
+module.exports = mongoose;
