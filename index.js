457d1847acb85640e2d964a82e6c5c37b692aba7
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-05-18 20:08:20 -0400

    creates index view

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var parser = require("body-parser")
+var express = require('express');
+var hbs = require('express-handlebars')
+
+var app = express();
+
+var Restaurant = mongoose.model("Restaurant");
+var MenuItem = mongoose.model("Menu");
+
+app.set("port", process.env.PORT || 3001);
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+  Restaurant.find({}).then(function(restaurant){
+      res.render("index", {
+      restaurants: restaurant
+    });
+  });
+});
+
+app.listen(app.get("port"), function(){
+  console.log("🦄 Magical");
+});
