b86085650e438eea11bc88054ff1a32cc46393c2
Leland Jordon <lelandjordon@mac.com>
2016-05-18 21:00:37 -0400

    app works, sends hello world to browser.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var express = require("express");
+var app = express();
+var hbs = require("express-handlebars");
+
+app.set('view engine', 'hbs');
+app.engine('.hbs', hbs({
+  extname:      '.hbs',
+  partialsDir:  'views/',
+  layoutsDir:   'views/',
+  defaultLyout: 'layout-main.hbs'
+}))
+
+app.get("/", function(req, res){
+  res.send("hey dude");
+});
+
+app.listen(4323, function(){     // makes the app listen to port 4444 from the browser
+  console.log("it works");      // at the moment, it works: http://127.0.0.1:4323/ and
+});                             // it shows "it works" in the terminal running nodemon.
