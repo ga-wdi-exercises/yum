33fb58f59ef8a9b0a7d92480da7439edc0ac5a9b
Matt <mvaden08@gmail.com>
2016-05-18 21:11:04 -0400

    Initial commit.

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,12 @@
 var Schema = require("../db/schema.js");
+var mongoose = require('mongoose');
+
+restaurants
+
+
+var restaurant1 = new Restaurant({name: "reminder1!!",});
+var restaurant2 = new Restaurant({name: "reminder2!!",});
+var restaurant3 = new Restaurant({name: "reminder3!!",});
+var restaurant4 = new Restaurant({name: "reminder4!!",});
+var restaurant5 = new Restaurant({name: "reminder5!!",});
+var restaurant6 = new Restaurant({name: "reminder6!!",});
