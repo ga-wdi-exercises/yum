86475ab14beffdd33a0815b9b2c38954fb5febfe
Matthew Scott <matthewruelscott@gmail.com>
2016-07-25 14:26:35 -0400

    Created controller

---
+++ controllers/restaurantsController.js
@@ -0,0 +1,35 @@
+var mongoose = require("mongoose");
+var Schema = require("../db/schema.js");
+var Restaurant = Schema.Restaurant;
+var Menu = Schema.Menu;
+
+var restaurantsController = {
+  index: function(){
+    Restaurant.find({}, function(err, restaurants){
+      if(err){
+        console.log(err);
+      } else {
+        console.log(restaurants);
+      }
+    })
+  },
+  show: function(req){
+    Restaurant.findOne({name: req.name}, function(err, restaurant){
+      if(err){
+        console.log(err);
+      } else {
+        console.log(restaurant);
+      }
+    })
+  },
+  update: function(req, update){
+    Restaurant.findOneAndUpdate({name: req.name}, {name: update.name}, {new: true}).then(function(restaurant){
+      console.log(restaurant);
+    })
+  },
+  destroy: function(req){
+    Restaurant.findOneAndRemove(req, function(restaurant){
+      console.log(restaurant);
+    })
+  }
+};
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,34 @@
+var mongoose = require("mongoose");
 var Schema = require("../db/schema.js");
+var Restaurant = Schema.Restaurant;
+var Menu = Schema.Menu;
+
+Restaurant.remove({}, function(err){
+  if(err){
+    console.log(err);
+  }
+});
+
+Menu.remove({}, function(err){
+  if(err){
+    console.log(err);
+  }
+});
+
+var ricebar = new Restaurant({name: "Rice Bar", address: {street: "1020 19th St NW, Washington, DC", zipcode: "20036"}, yelpUrl: "http://www.yelp.com/biz/rice-bar-washington"});
+
+var item1 = new Menu({title: "Noodle Bowl"});
+
+var restaurants = [ricebar];
+var items = [item1];
+
+for(var i=0; i < restaurants.length; i++){
+  restaurants[i].items.push(items[i]);
+  restaurants[i].save(function(err, restaurant){
+    if(err){
+      console.log(err);
+    } else{
+      console.log(restaurant);
+    }
+  })
+};
