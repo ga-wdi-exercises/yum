1cb5f3dcd90f6c152a4d513d56bcd09d21e751cd
Matthew Scott <matthewruelscott@gmail.com>
2016-07-26 14:28:21 -0400

    Added views

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+var express = require("express");
+var hbs = require("express-handlebars");
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var app = express();
+
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}));
+app.use("/assets", express.static("public"));
+
+app.get("/", function(req, res){
+  res.render("app-welcome");
+});
+
+app.get("/restaurants", function(req, res){
+  res.render("restaurants-index", {
+    numRestaurants: 32
+  });
+});
+
+app.listen(3001, function(){
+  console.log("It's aliiive!");
+});
