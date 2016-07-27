ad2fd433d7a61642637ac1d325c92a384c4514db
Kristin Pasive <kristin.pasive@gmail.com>
2016-07-27 19:37:27 -0400

    completed part 1

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+var express = require("express");
+var hbs = require("express-handlebars");
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var app = express();
+
+
+app.get("/", function(req, res){
+  res.send("Hello, world!");
+});
+
+app.listen(3001, function(){
+  console.log("It's alive!!!");
+});
