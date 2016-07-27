3d4b8e9cc58913a5b952bc8aa10118b86db657cb
Thaisun Green <Tlgreen16@gmail.COM>
2016-07-27 17:33:57 -0400

    1st Step

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,25 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId
+
+
+var RestrauntSchema = new Schema({
+  name: String,
+  address: ({street: String, zipcode: Number}),
+  yelpUrl: String,
+  items: [{type: Schema.ObjectId, ref: "Menu"}]
+})
+var MenuSchema = new Schema({
+  title: String
+})
+
+ var Restraunt = mongoose.model("Restraunt", RestrauntSchema);
+// var Menu = mongoose.model("Menu", MenuSchema);
+// module.exports = {
+//   Menu: Menu,
+//   Restraunt: Restraunt
+// };
+var mikes = new Restraunt({name:"mikes",address:({street:"1245 15th, Alexandria, Va", zipcode: 23671}),yelpUrl:"yelp.com" })
--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var express = require('express');
+var app = express();
+
+app.listen(8008);
+app.get("/",function(res,req){
+
+});
