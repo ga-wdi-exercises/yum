c5b37ee5755d9216596cc677c68e1b77e0a87ff6
James Hughes <heezmagnif@gmail.com>
2016-07-27 14:30:47 -0400

    initial commit & got page showing

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,28 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+
+
+var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId;
+
+var MenuItemSchema = new Schema({
+  title: String
+})
+
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: { street: String, zipcode: Number },
+  yelpUrl: String,
+  items: [MenuItemSchema]
+});
+
+var MenuItemModel = mongoose.model("MenuItem", MenuItemSchema);
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+
+module.exports = {
+  Restaurant: RestaurantModel,
+  Menu: MenuItemModel
+};
--- index.js
+++ index.js
@@ -1,2 +1,94 @@
 var mongoose = require('mongoose');
-var Schema = require("./db/schema.js");
+var Schema = require('./db/schema.js');
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+app.get("/", function(req, res) {
+  res.send('Hello World')
+})
+
+
+
+var Restaurant = Schema.Restaurant;
+var MenuItem = Schema.MenuItem;
+
+Restaurant.create({ name: 'Chabubbly', address: 'Landover St.', zipcode: 22032 }, function (err, restaurant) {
+  if (err) return handleError(err);
+})
+
+function findByName(restaurant) {
+  Restaurant.findOne({ name: restaurant }, function(err, restaurant) {
+    if (err){
+      console.log(err);
+    }
+    else{
+      console.log(restaurant);
+    }
+  });
+}
+
+function findByZip(zipcode) {
+  Restaurant.find({}), function(err, docs) {
+    if (err){
+      console.log(err);
+    }
+    else{
+      console.log(zipcode);
+    }
+  }
+}
+
+function update(restaurant, update) {
+  Restaurant.findOneAndUpdate({name: restaurant}, {name: update}, {new: true}, function(err, doc) {
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(doc);
+    }
+  })
+}
+
+  function destroy(restaurant) {
+    Restaurant.findOneAndRemove({name: restaurant}, function (err, docs) {
+      if(err){
+        console.log(err);
+      }
+      else {
+        console.log(docs);
+      }
+    })
+  }
+
+
+function addItem(restaurant, item){
+  Restaurant.findOne({name: restaurant}, function(err, docs) {
+    docs.item.push(new MenuItem({title: item}))
+    docs.save(function (err, results) {
+      if (err) {
+        console.log(err);
+      }
+        else {
+        console.log(results);
+        }
+      })
+    });
+  }
+
+function removeItem(restaurant, item) {
+  Restaurant.findOneAndUpdate({name: restaurant}, {
+     'pull': { items: {title: item} }
+   }, { 'new': true },
+   function(err, docs){
+     if(err){
+       console.log(err);
+     }
+     else {
+       console.log(docs);
+     }
+   });
+}
