45fffd1d30bf36cd69d15fee96b8167483a58bf1
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-07-27 19:48:53 -0400

    switched branches

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
+
+
+// Need to get server working
+
+
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+// var app = express()
+//
+// app.get("/:name", function(res, req){
+//   res.send(req.params.name)
+// })
+//
+// app.listen(process.env.PORT || 3001, function(){
+//   console.log("It's aliiive!");
+// })
+
+
+var Place = Schema.Place
+var Menu = Schema.Menu
+
+Model.find({}).then(function(model){
+  console.log(model)
+})
+
+//create a new restaurant
+Place.create({name: "h", address: {street: "pleasant street", zipcode:20001}, yelpUrl:"z"},
+function (err, place) {
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log(place)
+  }
+})
+
+Place.findOne({name: state.params.name}, function (err, place) {
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log(place)
+  }
+})
+
+Place.findAll({})
