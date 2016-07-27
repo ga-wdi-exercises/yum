4535214a75a207d2796eb4df7803a02b10d3bd1f
Julia Najafi <Julia.Najafi@gmail.com>
2016-07-27 10:05:33 -0400

    added schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,21 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var ItemSchema = new Schema({
+  title: String
+})
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [ItemSchema]
+})
