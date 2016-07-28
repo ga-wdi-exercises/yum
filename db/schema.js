aea8f23336c6c2d64a6b7b7726ae29db4d874de7
Michael L Parks <github@wmi-inc.com>
2016-07-27 20:41:55 -0400

    Attempted to Seed with no success

--- db/schema.js
+++ db/schema.js
@@ -1,8 +1,24 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId
+
+var ItemsSchema = new Item({
+  title: String
+});
+
+var RestaurantSchema = new Restaurant({
+  name: String,
+  address:{
+    street: String,
+    zipcode: Number
+  },
+  yelp_url: String,
+  items:[ItemSchema]
+});
 
 db.once('open', function() {
   console.log("database has been connected!");
