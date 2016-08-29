6f5a8992bd2f922131a18a146d085f86941bdb37
General Assembly <generalassembly@Generals-MacBook-Air-7.local>
2016-08-29 19:12:22 -0400

    Added dependencies.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var express = require("express")
+var bodyParser = require("body-parser")
+var methodOverride = require('method-override')
+
+var restaurantsController = require("./controllers/restaurantsController")
+mongoose.connect('mongodb://localhost/yum')
+
+var app = express()
+
+app.set("view engine", "hbs")
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(methodOverride('_method'))
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(4000, function(){
+  console.log("app listening on 4000")
+})
+
+app.get("/restaurants", restaurantsController)
