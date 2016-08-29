d0718a32318c24a75125bb79baf57b874c0c4c9c
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-08-29 18:20:24 -0400

    created schema and seed files. not seeding for some reason

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,25 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId
+
+var MenuItemSchema = new Schema({
+  title: String
+});
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: String,
+  yelpUrl: String,
+  items: [MenuItemSchema]
+});
+
+var MenuItemModel = mongoose.model("MenuItemSchema", MenuItemSchema);
+var RestaurantModel = mongoose.model("Restaurant", RestaurantSchema);
+
+module.exports = {
+  Restaurant: RestaurantModel,
+  MenuItem: MenuItemModel
+};
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,37 @@
-var Schema = require("../db/schema.js");
+var mongoose = require("mongoose");
+var Schema = require("./schema.js");
+
+var Restaurant = Schema.Student
+var MenuItem = Schema.MenuItem
+
+Restaurant.remove({}, function(err){
+  console.log(err)
+})
+MenuItem.remove({}, function(err){
+  console.log(err)
+})
+
+var FastEddies = new Restaurant({name: "Fast Eddies", address: "123 Fake St", yelpUrl: "https://www.yelp.com/biz/fast-eddies-billards-morningside"})
+var Chipotle = new Restaurant({name: "Chipotle", address: "36 Chambers Way", yelpUrl: "https://www.yelp.com/biz/chipotle-mexican-grill-washington-14"})
+var McDonalds = new Restaurant({name: "McDonalds", address: "3800 Powell Ln", yelpUrl: "https://www.yelp.com/biz/mcdonalds-washington"})
+
+var item1 = new MenuItem({title: "Shrimp Flambe"})
+var item2 = new MenuItem({title: "Seared Foi Gras"})
+var item3 = new MenuItem({title: "Salmon on Plank"})
+var item4 = new MenuItem({title: "Lobster Bisque"})
+var item5 = new MenuItem({title: "Popcorn"})
+var item6 = new MenuItem({title: "Vegan Tacos"})
+
+var restaurants = [FastEddies, Chipotle, McDonalds]
+var menuItems = [item1, item2, item3, item4, item5, item6]
+
+for(var i = 0; i < restaurants.length; i++){
+  restaurants[i].menuItems.push(menuItems[i], menuItems[i+3])
+  restaurants[i].save(function(err, resturant){
+    if(err){
+      console.log(err)
+    } else {
+      console.log(resturant);
+    }
+  })
+};
