e4544ad4b27a6577833df9eabb75eba3d89a6e8c
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-08-29 08:32:39 -0400

    Added a method that is supposed to add menu items to each restaurant.

--- db/schema.js
+++ db/schema.js
@@ -27,17 +27,34 @@ var restaurant1 = new Restaurant({"name": "Bertucci's", "address": {"2000 Pennsy
 var menu1 = new Menu({"title": "Shrimp Alfredo"});
 
 //Find a restaurant by name
-db.restaurants.find({"name": "Bertucci's"});
+function findName(restaurants){
+db.restaurants.findOne({"name": "Bertucci's"});
+}
 
 //Find all restaurants by zipCode
+function findZip(restaurants, zipCode){
 db.restaurants.find({"address.zipCode": 20006});
-
+}
 // Create a function that updates a restaurant.
-db.restaurants.update({"name": "Bertucci's"},
-"name": "Bertucci's",
-"address": {
-  "street": "2000 Pennsylvania Ave",
-  "zipCode": 20006
-},
-"yelpUrl": "https://www.yelp.com/biz/bertuccis-washington-3?osq=Bertucci%27s"
+
+db.restaurants.updateOne({"name": "Bertucci's"},
+{
+$set: {"name": "Bertuccis"}
+}
 );
+
+//Write a function or method that deletes a restaurant.
+
+db.restaurants.remove({"name": "Bertuccis"})
+
+// Write methods to add and remove embedded menu item documents for a restaurant of your choosing.
+for(var i = 0; i < restaurants.length; i++){
+  restaurants[i].menus.push(menus[i], menus[i+1])
+  restaurants[i].save(function(err, restaurant){
+    if (err){
+      console.log(err);
+    } else {
+      console.log(restaurant);
+    }
+  })
+};
