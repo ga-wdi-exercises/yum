83aa6a208f9f2b79626b32a23b95bea236a46498
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-08-28 23:58:49 -0400

    completed part 1

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,32 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function(){
+  console.log("db connected and working");
+});
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.objectId;
+
+var ItemSchema = new Schema({
+  title: String
+});
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [ItemSchema]
+});
+
+var Item = mongoose.model("Item", ItemSchema);
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+
+module.exports = {
+  Restaurant: Restaurant,
+  Item: Item
+};
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,35 @@
 var Schema = require("../db/schema.js");
+var mongoose = require("mongoose");
+
+var Item = Schema.Item
+var Restaurant = Schema.Restaurant
+
+Restaurant.remove({}, function(err){
+  console.log(err)
+});
+
+Item.remove({}, function(err){
+  console.log(err)
+});
+
+var item1 = new Item({title: "BLT"})
+var item2 = new Item({title: "Pie"})
+var item3 = new Item({title: "PBJ"})
+var item4 = new Item({title: "WTF"})
+
+var diner1 = new Restaurant({name: "Diner#1", address: {street: "123 Random St", zipcode: 12345}, yelpUrl: "www.yelp.com"})
+var diner2 = new Restaurant({name: "Diner#2", address: {street: "876 Derp St", zipcode: 54321}, yelpUrl: "www.yelp.com"})
+
+var restaurants = [diner1, diner2]
+var items = [item1, item2, item3, item4]
+
+for(var i = 0; i < restaurants.length; i++){
+  restaurants[i].items.push(items[i], items[i+1])
+  restaurants[i].save(function(err, restaurant){
+    if(err){
+      console.log(err);
+    } else {
+      console.log(restaurant);
+    }
+  })
+};
