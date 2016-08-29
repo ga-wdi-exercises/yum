574329326852b80a541b8942768a87049d0ab780
pamelaolney <pjolney12@gmail.com>
2016-08-29 08:48:26 -0400

    updated

--- db/schema.js
+++ db/schema.js
@@ -8,8 +8,35 @@ var ObjectId = Schema.ObjectID
 
 var RestaurantSchema = new Schema({
   name: String,
-  address: String,
+  address: [street: String, zipcode: Number],
   yelpUrl: String,
-  items: String,
+  items: [MenuItemSchema]
+});
+
+var MenuItemSchema = new Schema({
   title: String
+})
+
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+var MenuItem = mongoose.model("MenuItem", MenuItemSchema);
+
+var foodies = new Restaurant({
+  name: "Foddies",
+  address: [street: "New York", zipcode: 10001]
+  yelpUrl: "www.foodies.com",
+  items: "food"
+})
+
+Restaurant.create({
+  name: "Dinner",
+  address: [street: "New York", zipcode: 10002]
+  yelpUrl: "www.dinner.com",
+  items: "food"
+}, function(err restaurant){
+  if (err){
+    console.log(err);
+  }
+  else{
+    console.log(student);
+  }
 });
