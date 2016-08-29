1e4682c2d7f7b251099039c80dc7a8b2501e110b
General Assembly <generalassembly@Generals-Air-7.home>
2016-08-28 22:51:18 -0400

    Not finished but can't test anything. So don't feel good about continuing.

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -2,7 +2,7 @@ var Schema = require("../db/schema.js")
 var Restaurant = Schema.Restaurant
 var MenuItem = Schema.MenuItem
 
-console.log(Restaurant);
+//console.log(Restaurant);
 
 var restaurantsController = {
   index: function() {
@@ -10,6 +10,27 @@ var restaurantsController = {
       console.log("Err: ", err);
       console.log(docs);
     });
+  },
+  showByName: function() {
+    Restaurant.findOne({name: req.name}, function(err, docs){
+      console.log(docs);
+    })
+  },
+  showByZip: function() {
+    Restaurant.findOne({address.zipcode: req.zipcode}, function(err, docs){
+      console.log(docs);
+    });
+
+  },
+  update: function() {
+    Restaurant.findOneAndUpdate({name: req.name}, {name: update.name}, {new: true}, function(err, docs){
+      if(err){
+        console.log(err);
+      }
+      else {
+        console.log(docs);
+      }
+    })
   }
 }
 restaurantsController.index();
