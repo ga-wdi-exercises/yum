27092d98e43441f6c46276b2e9fade2c3b2f7553
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-08-29 04:59:46 -0400

    Created schemas for corresponding models for a restaurant and menu items. Added seed data for the restaurant and menu items

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,17 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Restaurant = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [{type: Schema.ObjectIdm ref: "MenuItems"}]
+})
+
+var Menu = new Schema ({
+  title: String
+})
