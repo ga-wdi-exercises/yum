75b3571fde34b0a7a6527027b4364ebd2349efe1
Winston Davis Mclean <winston.davis.mclean@gmail.com>
2016-08-29 13:22:47 -0400

    i don't know, it just wont work

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// REQUIRED FILES \\
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var Restaurant = Schema.Restaurant;
+
+console.log(Restaurant);
+Restaurant.create({
+  name: "The Fuck",
+  address: {
+    street: "main",
+    zipcode: 90210
+  },
+  yelpUrl: "http://wtfsigte.com/"
+});
+
+console.log(Restaurant.findOne({name: "The Fuck"}));
+  
+
