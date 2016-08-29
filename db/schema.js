b9b92efe85102350e49a3183fdb938b66f6d49c2
Alexander P. Holland <alexanderpholland@Alexanders-MacBook-Air.local>
2016-08-29 18:28:52 -0400

    defined schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,14 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function(){
+  console.log("Database Connected.");
+});
+
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId
+
+var RestaurantSchema = newSchema({
+  title: String
+});
