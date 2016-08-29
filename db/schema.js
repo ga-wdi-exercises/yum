fb349ed908159cd0b8d0bef8267127c27aa3f499
pamelaolney <pjolney12@gmail.com>
2016-08-29 08:29:36 -0400

    working on initial set up

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,14 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectID
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: String,
+  yelpUrl: String,
+  items: String,
+  title: String
+});
--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/yum')
 var Schema = require("./db/schema.js");
+
+var db = mongoose.connection;
+db.on('error', function(err){
+  console.log(err);
+});
+
+  db.once('open', function() {
+    console.log("database has been connected");
+});
