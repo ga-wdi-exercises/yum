f12a66075113736acabb358e3f21142b26ba0989
pamelaolney <pjolney12@gmail.com>
2016-08-30 10:57:36 -0400

    I am able to view all as well as show Restaurant

--- db/seed.js
+++ db/seed.js
@@ -1,42 +1,10 @@
-var Schema = require("../db/schema.js");
+var mongoose = require('./connection');
+var seedData = require('./seeds');
 
-var mongoose = require('mongoose');
-var Schema = require("./schema.js");
+var Restaurant = mongoose.model("Restaurant");
 
-var Restaurant = Schema.Restaurant
-var MenuItem = Schema.MenuItem
-
-// First we clear the database of existing restaurants and menuitems.
-Restaurant.remove({}, function(err){
-  console.log(err)
-});
-
-MenuItem.remove({}, function(err){
-  console.log(err)
-});
-
-var becky = new Restaurant({name: "becky"})
-var brandon = new Restaurant({name: "brandon"})
-var tom = new Restaurant({name: "tom"})
-
-var menuitem1 = new MenuItem({title: "menuitem1!!"})
-var menuitem2 = new MenuItem({title: "menuitem2!!"})
-var menuitem3 = new MenuItem({title: "menuitem3!!"})
-var menuitem4 = new MenuItem({title: "menuitem4!!"})
-
-var restaurants = [diner, foodies, delish]
-var menuitems = [menuitem1, menuitem2, menuitem3, menuitem4]
-
-for(var i = 0; i < restaurants.length; i++){
-  restaurants[i].menuitems.push(menuitems[i], menuitems[i+1])
-  restaurants[i].save(function(err, restaurant){
-    if (err){
-      console.log(err)
-    } else {
-      console.log(restaurant);
-    }
+Restaurant.remove({}).then(function(){
+  Restaurant.collection.insert(seedData).then(function(){
+    process.exit();
   })
-};
-
-Restaurant.findOne({name: "foodies"}, callback)
-Restaurant.findOne({zipcode: ""}, callback)
+})
--- index.js
+++ index.js
@@ -5,8 +5,22 @@ var Restaurant = mongoose.model("Restaurant");
 var bodyParser = require("body-parser");
 var app = express();
 
-app.set("port", process.env.PORT || 8000);
+app.listen(8000, function(){
+  console.log("I'm here at 8000");
+})
 
 app.get("/", function(req, res){
-  res.json()
+    res.json("Welcome to the the Restaurant Finder");
 });
+
+app.get("/restaurants", function(req, res){
+  Restaurant.find({}).then(function(restaurant){
+    res.json(restaurant);
+  })
+});
+
+app.get("/restaurants/:name", function(req, res){
+  Restaurant.findOne({name: req.params.name}).then(function(restaurant) {
+    res.json(restaurant);
+  })
+})
