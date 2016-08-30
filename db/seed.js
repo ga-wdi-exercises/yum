5f325bf8c2ccee20ccf9ea38f79c26df36afb1cb
Megan Coyle <meg@mcoyle.com>
2016-08-29 23:51:09 -0400

    Complete hw, have yet to add menu add/delete

--- db/seed.js
+++ db/seed.js
@@ -1,27 +1,13 @@
-var mongoose = require('mongoose');
-var Schema = require("../db/schema.js");
+var mongoose  = require("./connection");
+var seedData  = require("./seeds");
+mongoose.Promise = global.Promise;
+var Restaurant = mongoose.model("Restaurant");
+var Menu = mongoose.model("Menu");
 
-var Restaurant = Schema.Restaurant
-var Menu = Schema.Menu
+Menu.remove({})
 
-var yummy = new Restaurant({name: "yummy", address: {street: "200 Way St", zipcode: "22301"}, yelpUrl: "http://www.yelp.com/yummy"});
-var delish = new Restaurant({name: "delish", address: {street: "400 Way St", zipcode: "20001"}, yelpUrl: "http://www.yelp.com/delish"});
-
-var menu1 = new Menu({title: "burger"});
-var menu2 = new Menu({title: "pasta and meatballs"});
-var menu3 = new Menu({title: "mac and cheese"});
-var menu4 = new Menu({title: "shrimp and grits"});
-
-var restaurants = [yummy, delish]
-var menus = [menu1, menu2, menu3, menu4]
-
-restaurants.forEach(function(restaurant, i){
-  restaurant.items.push(menus[i], menus[i+3])
-  restaurant.save(function(err){
-    if (err) {
-      console.log(err)
-    } else {
-      console.log("Restaurant saved!");
-    }
-  })
-})
+Restaurant.remove({}).then(function(){
+  Restaurant.collection.insert(seedData).then(function(){
+    process.exit();
+  });
+});
--- index.js
+++ index.js
@@ -1,68 +1,139 @@
-var mongoose = require('mongoose');
-var Schema = require("./db/schema.js");
+var express = require("express");
+var parser = require("body-parser");
+var methodOverride = require('method-override')
+var hbs = require("express-handlebars");
+var mongoose = require("./db/connection");
+// mongoose.Promise = global.Promise;
 
-var Restaurant = Schema.Restaurant;
-var Menu = Schema.Menu;
+var Restaurant = mongoose.model("Restaurant");
+var Menu = mongoose.model("Menu");
 
+var app = express();
 
-function newRestaurant(name, street) {
-  Restaurant.create({ name: name, "address.street": street}, function(err, restaurant){
-    if (err) return handleError(err);
-    console.log("Restaurant created ", restaurant.name)
-  })
-}
+// var Restaurant = Schema.Restaurant;
+// var Menu = Schema.Menu;
 
-function findName(restaurant) {
-  Restaurant.findOne({name: restaurant}, function(err, result){
-      console.log(result);
-  });
-}
+app.set("port", process.env.PORT || 8080);
+app.set("view engine", "hbs");
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialsDir:    "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main"
+}));
+app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
 
-function findZipcode(zipcode) {
-  Restaurant.find({}, function(err, docs){
-    var codeResults = [];
-    docs.forEach(function(res) {
-      if (res.address.zipcode == zipcode){
-        codeResults.push(res);
-      }
+app.get("/", function(req, res){
+  Restaurant.find({}).then(function(restaurants){
+    res.render("index", {
+      restaurants: restaurants
     });
-    console.log(codeResults);
-  })
-}
-
-function updateRestaurantName(name, update) {
-  Restaurant.findOneAndUpdate({name: name}, {name: update}, {new:true},
-  function(err, docs){
-    console.log(docs);
-  })
-}
-
-function deleteRestaurant(name) {
-  Restaurant.findOneAndRemove({"name": name}, function(err, docs){
-    console.log(docs);
   });
-  console.log("Deleted a restaurant");
-}
+});
 
-function addMenuItems(name, item){
-  Restaurant.findOne({"name": name}, function(err, docs){
-    docs.items.push(new Menu({title: item}))
-    docs.save(function(err, results){
-      console.log(results)
+app.get("/:name", function(req, res){
+  var name = req.params.name
+  Restaurant.findOne({name: name}).then(function(restaurant){
+    res.render("show", {
+      restaurant: restaurant
+    });
+  });
+});
+
+app.post("/", function(req, res){
+  Restaurant.create(req.body).then(function(restaurant){
+    res.redirect("/" + restaurant.name);
+  });
+});
+
+app.delete("/:name/delete", function(req, res){
+  Restaurant.findOneAndRemove({name: req.params.name}).then(function(){
+    res.redirect("/")
+  });
+});
+
+app.put("/:name", function(req, res){
+  Restaurant.findOne({name: req.params.name}).then(function(restaurant) {
+    restaurant.name = req.body.name;
+    restaurant.save(function(err){
+      if(!err){
+        res.redirect("/" + restaurant.name);
+      }
     })
   });
-}
+});
 
-function removeMenuItems(restaurant, item){
-    Restaurant.findOneAndUpdate({name: restaurant}, {
-      $pull: {items: {title: item}}
-    },
-    {new: true}, function(err, docs) {
-      console.log(docs)
-  });
-}
+app.listen(app.get("port"), function(){
+  console.log("Time for some restauranting");
+});
 
-// removeMenuItems("Yum, Yum", "Hame and cheese sandwich")
+
+
+
+//
+// function newRestaurant(name, street) {
+//   Restaurant.create({ name: name, "address.street": street}, function(err, restaurant){
+//     if (err) return handleError(err);
+//     console.log("Restaurant created ", restaurant.name)
+//   })
+// }
+//
+// function findName(restaurant) {
+//   Restaurant.findOne({name: restaurant}, function(err, result){
+//       console.log(result);
+//   });
+// }
+//
+// function findZipcode(zipcode) {
+//   Restaurant.find({}, function(err, docs){
+//     var codeResults = [];
+//     docs.forEach(function(res) {
+//       if (res.address.zipcode == zipcode){
+//         codeResults.push(res);
+//       }
+//     });
+//     console.log(codeResults);
+//   })
+// }
+//
+// function updateRestaurantName(name, update) {
+//   Restaurant.findOneAndUpdate({name: name}, {name: update}, {new:true},
+//   function(err, docs){
+//     console.log(docs);
+//   })
+// }
+//
+// function deleteRestaurant(name) {
+//   Restaurant.findOneAndRemove({"name": name}, function(err, docs){
+//     console.log(docs);
+//   });
+//   console.log("Deleted a restaurant");
+// }
+//
+// function addMenuItems(name, item){
+//   Restaurant.findOne({"name": name}, function(err, docs){
+//     docs.items.push(new Menu({title: item}))
+//     docs.save(function(err, results){
+//       console.log(results)
+//     })
+//   });
+// }
+//
+// function removeMenuItems(restaurant, item){
+//     Restaurant.findOneAndUpdate({name: restaurant}, {
+//       $pull: {items: {title: item}}
+//     },
+//     {new: true}, function(err, docs) {
+//       console.log(docs)
+//   });
+// }
+
+
+
+
+// removeMenuItems("Yum, Yum", "Ham and cheese sandwich")
 // addMenuItems("Yum, Yum", "Ham and cheese sandwich")
 // deleteRestaurant("Yum, Yum");
 // newRestaurant("okay", "400 way st")
