707474d91d2ef020741bf5eea8595150672d9aeb
pamelaolney <pjolney12@gmail.com>
2016-08-30 12:13:34 -0400

    create works!

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var mongoose = require('./db/connection');
 var Restaurant = mongoose.model("Restaurant");
 
 var bodyParser = require("body-parser");
-var app = express();
 
+var app = express();
+app.use(bodyParser.json({extended: true}));
 app.listen(8000, function(){
   console.log("I'm here at 8000");
 })
@@ -13,14 +14,27 @@ app.get("/", function(req, res){
     res.json("Welcome to the the Restaurant Finder");
 });
 
+//displays all restaurants
+
 app.get("/restaurants", function(req, res){
   Restaurant.find({}).then(function(restaurant){
     res.json(restaurant);
   })
 });
 
+//shows individual restaurants by name in params
+
 app.get("/restaurants/:name", function(req, res){
   Restaurant.findOne({name: req.params.name}).then(function(restaurant) {
+    res.json(restaurant);
+  })
+})
+
+//creates new individual restaurant
+
+app.post("/restaurants", function(req, res){
+  console.log(req.body);
+  Restaurant.create(req.body.restaurant).then(function(restaurant){
     res.json(restaurant);
   })
 })
