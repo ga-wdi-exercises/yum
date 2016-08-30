c09ae8708bff3b6ad5bc8bf97e565d7341da2095
Taylor Edwards <tedwards1884@gmail.com>
2016-08-29 20:17:12 -0400

    hw submission

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-var mongoose = require('mongoose');
-var Schema = require("./db/schema.js");
+var mongoose = require("./db/schema.js");
+var express = require("express");
+var hbs = require("hbs");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var Restaurant = mongoose.model("RestaurantModel");
+var Menu = mongoose.model("MenuModel");
+
+var app = express();
+
+app.set("view engine", "hbs");
+app.listen(4000, function(){
+  console.log("good to go on 4000");
+})
+
+app.get("/restaurants", function(req, res){
+  Restaurant.find({}, function(err, docs){
+    res.render("restaurants-index", {
+      restaurants: docs
+    })
+  })
+})
+
+app.get("/restaurants/:id", function(req, res){
+  Restaurant.findById(req.params.id, function(err, doc){
+    res.render("restaurant-show", {
+      restaurant: doc
+    })
+  })
+})
