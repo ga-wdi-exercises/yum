c1b7ef21061a1632f6d259a8e7b46eb9164b224a
Domonique Griffin <griffind2009@gmail.com>
2016-11-08 18:25:45 -0500

    working on the functions

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,8 @@
-var Schema = require("../db/schema.js");
+var Schema = require("./db/schema.js");
+var mongoose = require("./models")
+var Restaurant = mongoose.model("Restaurant")
+
+
+Restaurant.create({name: "Uniontown Grill", address: {street: "111 Eat St" , zipcode: 20009 }, yelpUrl: "www.eat.com"}, items: [MenuSchema])
+Menu.create({title: "Uniontown Grill"})
+itmes.push(Menu)
--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+
+app.get("/restaurants/:name", function(req, res){
+Restaurant.findOne({name: req.params.name}).then(restaurant => {
+  res.render("show", {
+    restaurant,
+    })
+  })
+})
+
+app.get("/restaurants/:zipcode", function(req, res){
+Restaurant.find({zipcode: req.params.zipcode}).then(restaurant => {
+  res.render("show", {
+    restaurant,
+    })
+  })
+})
+
+app.update("/restaurant/:name", function(req, res) {
+  Restaurant.findOneAndUpdate({name: req.params.name}, {new: true}, {req.restaurant})
+  res.redirect("/restaurants/:name")
+})
