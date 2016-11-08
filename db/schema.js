d6734dddcedad30b0301977c0c835ea0ff576198
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-11-08 17:31:30 -0500

    created schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,16 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+
+var MenuItemSchema = new Schema ({
+  title: String
+})
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {street: String, zipcode: Number},
+  yelpUrl: String,
+  items = [MenuItemSchema]
+})
