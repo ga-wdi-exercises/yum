1069109387d352cf37ad882b7aa9c2d8b4fa551a
Tim Foley <thetimfoley@gmail.com>
2016-11-09 18:22:06 -0500

    initial commit

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,24 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var menuSchema = mongoose.Schema({
+  title: String
+})
+
+var restaurantSchema = mongoose.Schema({
+  name: String,
+  address: {street: String, zipcode: Number},
+  yelpUrl: String,
+  items: [menuSchema]
+})
+
+
+var Restaurant = mongoose.model('Restaurant', restaurantSchema)
+var Menu = mongoose.model('Menu', menuSchema)
+
+
+module.exports = {
+  Restaurant,
+  Menu,
+}
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,7 @@
-var Schema = require("../db/schema.js");
+var Schema = require("../db/schema");
+var Restaurant = Schema.Restaurant
+var Menu = Schema.Menu
+
+var cptn = new Restaurant({name: "Captain Cookie", address: {street: "Dupont Circle", zipcode: 20036}, yelpUrl: "http://www.yelp.com/biz/captain-cookie-and-the-milk-man-washington-5"})
+var bdc = new Restaurant({name: "Blind Dog Cafe", address: {street: "944 Florida Ave", zipcode: 20001}, yelpUrl: "http://www.yelp.com/biz/the-blind-dog-cafe-washington-2?osq=cookies"})
+var birch = new Restaurant({name: "Birch & Barley", address: {street: "1337 14th St NW", zipcode: 20001}, yelpUrl: "http://www.yelp.com/biz/birch-and-barley-washington?osq=Restaurants+cookies"})
