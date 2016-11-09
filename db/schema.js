7d4c2589e2e6c76e2ce80be3b75de8ad9c7ecd33
RYANBROPHY <BROPHY2321@ME.COM>
2016-11-09 15:57:13 -0500

    add schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,15 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+
+var RestaurantSchema = new Schema ({
+      name: String,
+      address: {street: String, zipcode: Number},
+      yelpUrl: String,
+      items: Array,
+})
+var MenuSchema = new Schema ({
+  title: String
+})
