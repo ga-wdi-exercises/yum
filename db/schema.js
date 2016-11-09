cc717c37b5337f8289dd297dfb780b02745f8ccd
William Keating <keating49@gmail.com>
2016-11-09 16:04:27 -0500

    schemas

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,24 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+
+var MenuSchema = mongoose.Schema({
+  title: String
+});
+
+var RestaurantSchema = mongoose.Schema({
+  name: String,
+  address: { street: String, zipcode: Number },
+  yelp: String,
+  items: [MenuSchema]
+});
+
+var MenuModel = mongoose.model("MenuSchema", MenuSchema);
+var RestaurantModel = mongoose.model("RestaurantSchema", RestaurantSchema);
+
+module.exports = {
+  Restaurant: RestaurantModel,
+  MenuItem: MenuItemModel
+};
