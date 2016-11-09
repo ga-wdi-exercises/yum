5c45b4256eb1a3a126ee51ee39f2eb0235420e63
Michael Lai <mike24567@gmail.com>
2016-11-08 20:03:01 -0500

    defined the models

--- db/schema.js
+++ db/schema.js
@@ -1,11 +1,13 @@
+//requires mongoose
 var mongoose = require('mongoose');
-
+//connects to mongodb
 mongoose.connect('mongodb://localhost/yum');
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 
+//defines the schemas
 var RestaurantSchema = new mongoose.Schema(
   {
     name: String,
@@ -23,3 +25,6 @@ var MenuSchema = new mongoose.Schema(
     title: String,
   }
 )
+//defines the models
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+var Menu = mongoose.model("Menu", MenuSchema);
