5bd605807a65e030e8dbe23b6deb13930c569ab3
Michael Lai <mike24567@gmail.com>
2016-11-08 19:53:49 -0500

    created menu and restaurant schema

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,25 @@
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/yum');
+
 var db = mongoose.connection;
+
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var RestaurantSchema = new mongoose.Schema(
+  {
+    name: String,
+    address: {
+      street: String,
+      zipcode: Number,
+    },
+    yelpurl: String,
+    items: [MenuSchema],
+  }
+);
+
+var MenuSchema = new mongoose.Schema(
+  {
+    title: String,
+  }
+)
