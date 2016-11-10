23e0f3822aec9ff8225535cbc2b73b8f35cd6a83
Kevin Mahoney <kjmahoney13@gmail.com>
2016-11-09 21:09:06 -0500

    part 1 complete

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,24 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var MenuItemSchema = new mongoose.Schema({
+  title: String,
+})
+
+var RestaurantSchema = new mongoose.Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number,
+  },
+  yelpUrl: String,
+  items: [MenuItemSchema],
+})
+
+mongoose.Promise = global.Promise
+
+module.exports = {
+  MenuItem: mongoose.model("MenuItem", MenuItemSchema),
+  Restaurant: mongoose.model("Restaurant", RestaurantSchema),
+}
