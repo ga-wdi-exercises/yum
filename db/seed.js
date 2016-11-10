20f7ff9ecaa1b534b744657829504da1475ff0d3
Andrew Whitley <awhitley1233@gmail.com>
2016-11-10 01:09:37 -0500

    finished part I & index view

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,59 @@
 var Schema = require("../db/schema.js");
+var MenuItem = Schema.MenuItem
+var Restaurant = Schema.Restaurant
+
+Restaurant.remove({}, () => {
+})
+MenuItem.remove({}, () => {
+})
+
+var chipotle = new Restaurant({
+  name: "Chipotle",
+  address: {
+    street: "1234 Burrito Way",
+    zipcode: 34567,
+  },
+  phone_number: "222.444.5555",
+})
+
+var burrito = new MenuItem({
+  name: "Burrito",
+  price: 7.99,
+})
+
+chipotle.items.push(burrito)
+chipotle.save()
+
+var mcdonalds = new Restaurant({
+  name: "McDonald's",
+  address: {
+    street: "8349 Burger Lane",
+    zipcode: 43567,
+  },
+  phone_number: "222.222.9999",
+})
+
+var cheeseburger = new MenuItem({
+  name: "Quarter Pounder",
+  price: 4.99,
+})
+
+mcdonalds.items.push(cheeseburger)
+mcdonalds.save()
+
+var chicfila = new Restaurant({
+  name: "Chic-fil-A",
+  address: {
+    street: "1234 Nugget Ave",
+    zipcode: 34533,
+  },
+  phone_number: "111.333.5555",
+})
+
+var nuggets = new MenuItem({
+  name: "12-piece Chicken Nuggets",
+  price: 5.99,
+})
+
+chicfila.items.push(nuggets)
+chicfila.save()
--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+var express = require('express')
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var MenuItem = Schema.MenuItem
+var Restaurant = Schema.Restaurant
+
+var app = express()
+
+app.set('view engine', 'hbs')
+
+
+app.get('/restaurants', (req, res) => {
+  Restaurant.find({}).then((restaurants) => {
+    res.render('index', { restaurants })
+  })
+})
+
+
+app.listen(3000, () => {
+  console.log('app listening on http://localhost:3000')
+})
