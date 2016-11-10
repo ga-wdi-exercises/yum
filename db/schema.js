2cf93c70857868cfdf42b6729cfa7851c1efd280
Latoya Watson <latoyamwatson@gmail.com>
2016-11-09 23:49:30 -0500

    initial setup

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,24 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/yum')
+// save the connection to the database in a variable
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = moongoose.Schema
+
+var menuSchema = new Schema({
+  title: String
+})
+
+var restaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number,
+  }
+  yelpUrl: String,
+  items:
+})
+
+module.exports = mongoose.model("Menu", menuSchema)
+module.exports = mongoose.model("Restaurant", restaurantSchema)
