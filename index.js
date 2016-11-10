18b07a0a92bfdbea793690587c6fabe5adccff47
Barrett Cope <barrettcope@gmail.com>
2016-11-09 21:51:50 -0500

    Done for the night

--- index.js
+++ index.js
@@ -5,12 +5,34 @@ var Schema = require("./db/schema.js")
 var Item = Schema.Item
 var Restaurant = Schema.Restaurant
 
-console.log("var mongoose: ",mongoose)
-console.log("var Schema: ",Schema)
-console.log("var Item: ",Item)
-console.log("var Restaurant: ",Restaurant)
+// console.log("var mongoose: ",mongoose)
+// console.log("var Schema: ",Schema)
+// console.log("var Item: ",Item)
+// console.log("var Restaurant: ",Restaurant)
 
 // 4
-function findByName(name) {
-  return
+function findByName(nameStr) {
+  Restaurant.findOne({name: nameStr}, function(err, result) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log(result)
+      return result
+    }
+  })
 }
+
+function findByZip(zip) {
+  Restaurant.find({zipcode: zip}, function(err, result) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log(result)
+      return result
+    }
+  })
+}
+
+console.log(findByName("Copycat"))
+// not working
+console.log(findByZip("20003"))
