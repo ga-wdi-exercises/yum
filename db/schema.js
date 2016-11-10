d457ca0cc7f996ba81bc1d5aaa25f3a2286578f4
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-11-09 19:37:18 -0500

    schema created

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,20 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+
+var ResterauntSchema = new Schema ({
+  name: String,
+  address: String,
+  yelpUrl: {street: String, zipcode: Number},
+  items: String
+});
+
+var Resteraunt = mongoose.model(Resteraunt, ResterauntSchema);
+
+module.exports = {
+  Resteraunt: Resteraunt
+};
