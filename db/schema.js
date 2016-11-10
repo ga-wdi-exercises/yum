502e165bb5389d73defd3875759d77e7454aa6a6
Khoi Le <khoileee89@gmail.com>
2016-11-09 20:25:41 -0500

    can find zipcodes

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,23 @@
 var mongoose = require('mongoose');
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+
+var MenuItemsSchema = mongoose.Schema({
+  title: String
+})
+var RestaurantSchema = mongoose.Schema({
+  name: String,
+  address: {street: String, zipcode: Number},
+  yelpUrl: String,
+  items: [MenuItemsSchema]
+})
+
+
+
+//model takes Two Arguments, 1) The singular name of the collection your model is for, and the Schema it uses.
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema)
+var Item = mongoose.model("Item", MenuItemsSchema)
+
 mongoose.connect('mongodb://localhost/yum');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+
+module.exports = {Item, Restaurant}
