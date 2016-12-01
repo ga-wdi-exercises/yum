dcf9b04ba4891cd9fe4c13edf7d70c52b5eea625
Sam Catherman <Sam.Catherman@gmail.com>
2016-12-01 07:55:54 -0500

    initial commit

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,25 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId
+
+var MenuSchema = mongoose.Schema({
+  title: String
+})
+
+var RestSchema = mongoose.Schema({
+  name: String,
+  address: {street: String, state: String, zip: Number},
+  yelp: String,
+  items: [MenuSchema]
+})
+
+mongoose.model("MenuSchema", MenuSchema);
+mongoose.model("Restaurant", RestaurantSchema)
+
+module.exports = {
+  Restaurant: RestaurantModel,
+  Menu: MenuModel
+}
--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var express = require("express");
+var app = express();
+
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+
+var Restaurant = Schema.Restaurant;
+var Menu = Schema.Menu;
+
+
+app.listen(4000, () => {
+  console.log("listening on port 4000");
+})
