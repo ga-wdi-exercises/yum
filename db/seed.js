a1085020a63d537fb71a00d5d85b9c871183245f
Drew Swinburne <drewswinburne@gmail.com>
2016-12-01 08:52:55 -0500

    completes homework no crud

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,16 @@
-var Schema = require("../db/schema.js");
+var Schema = require("./schema.js");
+
+var Restaurant = Schema.Restaurant
+var MenuItem = Schema.MenuItem
+
+
+Restaurant.remove({}, err => {
+  if(err){
+    console.log(err)
+  }
+});
+MenuItem.remove({}, err => {
+  if(err){
+    console.log(err)
+  }
+});
