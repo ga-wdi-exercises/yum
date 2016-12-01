726ec46eb3755ed7e833a85ba983e14db790c231
Thinh Nguyen <nguyendthinh@outlook.com>
2016-11-30 21:52:08 -0500

    begin setting up express

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+var Restaurant = Schema.Restaurant;
+var Menu = Schema.Menu;
+
+app.set("port", process.env.PORT || 4000);
+app.set("view engine", "hbs");
+
+app.use("/assets", express.static("public"));
+aap.use(bodyParser.urlencoded({extended:true}));
