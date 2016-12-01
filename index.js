44dddc12afba5c054f95dfb184d702642bfa9ee5
Thinh Nguyen <nguyendthinh@outlook.com>
2016-11-30 22:33:15 -0500

    successfully created new function

--- index.js
+++ index.js
@@ -7,8 +7,23 @@ var bodyParser = require("body-parser");
 var Restaurant = Schema.Restaurant;
 var Menu = Schema.Menu;
 
-app.set("port", process.env.PORT || 4000);
 app.set("view engine", "hbs");
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), function(){
+  console.log("WORK");
+})
 
 app.use("/assets", express.static("public"));
-aap.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+  Restaurant.find({}).then(restaurants => {
+    res.render("index", {restaurants});
+  });
+});
+
+app.post("/", (req, res) => {
+  Restaurant.create(req.body.restaurant).then(restaurant => {
+    res.redirect("/");
+  });
+})
