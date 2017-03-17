be314c72404ab62f1ee66ac04ff2237229bd8478
Collin OConnell <ococncol@gmail.com>
2017-03-17 14:07:16 -0400

    created schema and models for restaurant   and menus

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,20 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelp: String,
+  items: [MenuSchema]
+});
+
+var MenuSchema = new Schema({
+  title: String
+});
