09907b910a57e8f09fd08b733599a70914d961b4
Hannah Schuster <hschuster9@gmail.com>
2017-03-20 19:26:33 -0400

    part 1

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,40 @@
 var Schema = require("../db/schema.js");
+
+var MenuItem = Schema.MenuItem
+var Restaurant = Schema.Restaurant
+
+MenuItem.remove({}, err => {
+  if(err){
+    console.log(err)
+  }
+})
+
+Restaurant.remove({}, err => {
+  if(err){
+    console.log(err)
+  }
+})
+
+
+var restaurant1 = new Restaurant({name: "Church Key", address: {street: "1337 14th St NW", zipcode: 20005}, yelpUrl: "https://www.yelp.com/biz/churchkey-washington"})
+var restaurant2 = new Restaurant({name: "Barcelona", address: {street: "1622 14th St NW", zipcode: 20005}, yelpUrl: "https://www.yelp.com/biz/barcelona-wine-bar-washington"})
+var restaurant3 = new Restaurant({name: "Slipstream", address: {street: "1333 14th St NW", zipcode: 20005}, yelpUrl: "https://www.yelp.com/biz/slipstream-washington?osq=bars"})
+
+var item1 = new MenuItem({title: "pizza"})
+var item2 = new MenuItem({title: "french fries"})
+var item3 = new MenuItem({title: "cheese"})
+
+var restaurants = [restaurant1, restaurant2, restaurant3]
+var menuItems = [item1, item2, item3]
+
+restaurants.forEach((restaurant, i) => {
+  restaurant.items.push(menuItems[i], menuItems[i + 1])
+  restaurant.save((err, restaurant) => {
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(restaurant)
+    }
+  })
+})
--- index.js
+++ index.js
@@ -1,2 +1,91 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var Seeds = require("./db/seed.js")
+
+var Restaurant = Schema.Restaurant
+var MenuItem = Schema.MenuItem
+
+
+Restaurant.create({name: "Medium Rare",  address: {street: "515 8th St SE", zipcode: 20005}, yelpUrl: "https://www.yelp.com/biz/medium-rare-washington-2"}, function(err, restaurant) {
+  restaurant.save((err, restaurant) => {
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(restaurant)
+    }
+  })
+})
+
+
+function findbyName(req){
+Restaurant.findOne({name: req.name}, (err, restaurant) => {
+  if(err){
+    console.log(err)
+  }
+    else{
+      console.log(restaurant)
+}
+})
+}
+
+function findByZip(req){
+  Restaurant.find({address: {zipcode: req.zipcode}}, (err, restaurant) =>{
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(restaurant)
+    }
+  })
+}
+
+function updateRestaurant(req){
+  Restaurant.findOneAndUpdate({name: req.name}, {name: update.name}, {new: true}, (err, restaurant)=>{
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(restaurant)
+    }
+  })
+}
+
+function deleteRestaurant(req){
+  Restaurant.findOneAndRemove(req, (err, docs)=> {
+    if(err){
+      console.log(err)
+    }
+    else {
+      console.log(docs)
+    }
+  })
+}
+
+
+function addMenuItem(req, item){
+  Restaurant.findOne({name: req.name}, (err, restaurant)=>{
+    restaurant.items.push(item({title: item}))
+    restaurant.save((err, restaurant)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log(restaurant)
+      }
+    })
+  })
+}
+
+function removeMenuItem(req, item) {
+  Restaurant.findOneAndUpdate({name: req.name}, {$pull: {items: {title: item}}
+  ((err, restaurant)=> {
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(restaurant)
+    }
+  })
+  })
+  }
