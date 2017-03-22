6b68dbcdfc1b9f64578392f1611e95b4de68ed8e
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-21 23:43:03 -0400

    seed functions added

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,28 @@
 var Schema = require("../db/schema.js");
+
+var MenuItem = Schema.Reminder
+var Restaurant = Schema.Restaurant
+
+MenuItem.remove({}, err => {
+  if (err){
+    console.log(err);
+  }
+})
+
+Restaurant.remove({}, err => {
+  if(err){
+    console.log(err)
+  }
+})
+
+
+restaurants.forEach(function(restaurant, i){
+  restaurant.menuitems.push(menuitems[i], menuitems[i+1]);
+  restaurant.save(function(err){
+    if (err){
+      console.log(err);
+    } else {
+    console.log("restaurant up")
+  }
+  })
+})
