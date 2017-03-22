5cc538d563c6d4fb381f800ced4eef371b7a5df6
Bedig Galladian <bgalladian@gmail.com>
2017-03-22 16:33:21 -0400

    added controller file and functions

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,32 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+  console.log("Praise Mongod")
+})
+
+var Schema = mongoose.Schema
+
+var MenuItemSchema = new Schema({
+  title: String
+})
+
+var RestaurantSchema = new Schema ({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [MenuItemSchema]
+
+})
+
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+var MenuItem = mongoose.model("MenuItem", MenuItemSchema)
+
+module.exports = {
+  Restaurant: Restaurant,
+  MenuItem: MenuItem
+}
