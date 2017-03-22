1f9ac6482644173d46e89bc0891de24d2f0cb1b1
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-22 19:31:52 -0400

    show function added in controller for name

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -3,16 +3,24 @@ var MenuItem = Schema.MenuItem;
 var Restaurant = Schema.Restaurant;
 
 var restaurantsController = {
-  index: function(){
+  index: function(req, res){
     Restaurant.find({}, function(err, docs){
       console.log(docs);
       return docs
     });
   },
+  show: function(req){
+    Restaurant.findOne({"name": req.name}, function(err, docs){
+      console.log(docs);
+      return docs;
+    });
+  }
 
 }
 
-restaurantsController.index(); 
+// restaurantsController.index();
+// restaurantsController.show({name: "chinaOne"});
+
 
 
 
