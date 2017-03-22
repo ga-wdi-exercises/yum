ccc9e1804080e007e23018957a285d20ed98383a
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-22 14:14:09 -0400

    controller set up

---
+++ controllers/restaurantsController.js
@@ -0,0 +1,12 @@
+var Schema = require("../db/schema.js");
+var MenuItem = Schema.MenuItem;
+var Restaurant = Schema.Restaurant;
+
+var restaurantsController = {
+
+}
+
+
+
+
+module.exports = restaurantsController; 
