edf63161dc12312e3f9a1647dfb731955abe6387
Nat Carter <n.goldman.carter@gmail.com>
2017-03-22 09:48:24 -0400

    worked on the mongoose problems

---
+++ mongooseOnly.js
@@ -0,0 +1,38 @@
+// 1. Create a new restaurant.
+Restaurant.create({
+  name: 'Burger Joint',
+  address: {
+    street: '789 Fictional Street',
+    zipcode: 20016
+  },
+  yelpUrl: 'https://www.yelp.com/third-fake-url',
+  items: [
+    {title: 'Burger 1'},
+    {title: 'Burger 2'}
+  ]
+})
+
+// 2. Write a function or method that finds a restaurant by `name`.
+function findRestaurantByName(name) {
+  return Restaurant.findOne({name})
+}
+
+// 3. Write a function or method that finds all restaurants by `zipCode`.
+function findRestaurantsByZipcode(zipcode) {
+  return Restaurant.find({zipcode})
+}
+
+// 4. Create a function that updates a restaurant.
+function updateRestaurant(name, objectLiteral) {
+  Restaurant.findOneAndUpdate(
+    {name},
+    objectLiteral
+  )
+}
+
+// 5. Write a function or method that deletes a restaurant.
+function deleteRestaurant(name) {
+  Restaurant.findOneAndRemove({name})
+}
+
+// 6. Write methods to add and remove embedded menu item documents for a restaurant of your choosing.
--- todo.md
+++ todo.md
@@ -1,15 +1,4 @@
-## Express
+Create a new menu item for the restaurant.
+Delete a menu item from the restaurant.
 
-Turn YUM into an Express app with routes that you can view in your browser and forms that manipulate your data with full CRUD.
-
-  - Create a new menu item for the restaurant.
-  - Delete a menu item from the restaurant.
-
-## Mongoose only
-
-  3. Create a new restaurant.
-  4. Write a function or method that finds a restaurant by `name`.
-  5. Write a function or method that finds all restaurants by `zipCode`.
-  6. Create a function that updates a restaurant.
-  7. Write a function or method that deletes a restaurant.
-  8. Write methods to add and remove embedded menu item documents for a restaurant of your choosing.
+Complete steps in mongooseOnly.js
