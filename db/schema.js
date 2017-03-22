a7ed9329906ad4eb6c03c89f7fc69459393ad934
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-22 13:53:39 -0400

    initial commit

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,23 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+
+var RestaurantSchema = new mongoose.Schema(
+  {
+    name: String,
+    address: {
+      street: String,
+      zipcode: Number,
+      }
+    yelpUrl: String
+    items: Array
+  }
+)
+
+var MenuSchema = new mongose.Schema(
+  {
+    title: String
+  }
+)
+
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
