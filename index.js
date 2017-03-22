d71ff771178a761b5299bfb5883a66d00438fb74
Eva Lai <evalai.dc@gmail.com>
2017-03-22 17:17:40 -0400

    added create restaurant functionality

--- index.js
+++ index.js
@@ -12,12 +12,32 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set("port", process.env.PORT || 3001);
 
-app.get('/', function(req, res){
-  console.log("hello")
-})
+app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended: true}));
 
+// lists all restaurants at home page.
+app.get('/', function(req, res){
+  Restaurant.find({}).then( (restaurants) => {
+  res.render("index", {restaurants})
+  })
+});
+
+
+// create new restaurant
+app.post('/', function(req, res){
+  Restaurant.create(req.body.restaurant).then( (err, restaurant) => {
+    res.redirect("/");
+  })
+});
+
+// show individual restaurant and their info
+app.get('/restaurants/:name', function(req, res){
+  Restaurant.findOne({name: req.params.name}).then(function(restaurant){
+    res.render('show', {restaurant})
+  })
+});
 
 
 app.listen(app.get("port"), function(){
   console.log('Alive and Active');
-})
+});
