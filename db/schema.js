07b56033ec897a6cf95175acab1c9812772f2067
Erica Norby <ericanorby@gmail.com>
2017-03-22 18:00:05 -0400

    created schemas and models

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,20 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var RestaurantSchema = new mongoose.Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [MenuItemSchema]
+})
+
+var MenuItemSchema = new mongoose.Schema({
+  title: String
+})
+
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema)
+var MenuItem = mongoose.model("MenuItem", MenuItemSchema)
