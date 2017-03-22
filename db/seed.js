0843ac28888884d9bc8c3cd1f66adb25e075d40a
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-03-22 14:02:25 -0400

    seeds added, connected

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,6 @@
 var Schema = require("../db/schema.js");
 
-var MenuItem = Schema.Reminder
+var MenuItem = Schema.MenuItem
 var Restaurant = Schema.Restaurant
 
 MenuItem.remove({}, err => {
@@ -15,9 +15,18 @@ Restaurant.remove({}, err => {
   }
 })
 
+var eggrolls = new MenuItem({title: "eggrolls"});
+var orangeChicken = new MenuItem({title: "orange chicken"});
+var chickenLoMein = new MenuItem({title: "chicken lomein"});
+
+var chinaOne = new Restaurant({name: "China One", address: "78 Shell Shore Road", zipcode: 12345, yelpUrl: "www.chinaone.com"});
+var greatWall = new Restaurant({name: "Great Wall", address: "123 Richmond Way", zipcode: 54321, yelpUrl: "www.greatwall.com"});
+
+var menuItems = [eggrolls, orangeChicken, chickenLoMein];
+var restaurants = [chinaOne, greatWall];
 
 restaurants.forEach(function(restaurant, i){
-  restaurant.menuitems.push(menuitems[i], menuitems[i+1]);
+  restaurant.items.push(menuItems[i], menuItems[i+1]);
   restaurant.save(function(err){
     if (err){
       console.log(err);
