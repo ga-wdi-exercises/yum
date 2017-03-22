c9df055c533e9c8557e97a901378b864032e1f60
Mike Nabil <maikel_nabil@yahoo.com>
2017-03-22 15:03:18 -0400

    created the schema

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,40 @@
+//Initializing Mongoose
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
+
+// Save that connection to the database in a variable.
 var db = mongoose.connection;
+
+// Will log an error if db can't connect to MongoDB
 db.on('error', console.error.bind(console, 'connection error:'));
+
+// Will log "database has been connected" if it successfully connects.
+db.once('open', () => {
+  console.log('database has been connected!');
+});
+
+//Schema
+var Schema = mongoose.Schema;
+
+var MenuItemSchema = new Schema({
+  title: String,
+});
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String
+  items: [MenuItemSchema]
+});
+
+//Models
+var Restaurant= mongoose.model('Restaurant', RestaurantSchema);
+var MenuItem = mongoose.model('MenuItem', MenuItemSchema);
+
+module.exports = {
+  Restaurant: Restaurant,
+  MenuItem: MenuItem
+};
