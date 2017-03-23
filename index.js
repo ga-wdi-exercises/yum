643a1b16536611239351be7cdffbed9c0d6328bf
Dominick Houston <dominick.houston14@gmail.com>
2017-03-22 20:41:16 -0400

    Code Code Code

--- index.js
+++ index.js
@@ -1,2 +1,64 @@
 var mongoose = require('mongoose');
-var Schema = require("./db/schema.js");
+var schema = require("./db/schema.js");
+
+var express = require("express");
+var parser  = require("body-parser");
+var hbs     = require("express-handlebars");
+var mongoose = require("./db/connection.js");
+
+var Restaurant = mongoose.model("Restaurant");
+var Menu = mongoose.model("Menu");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3001);
+app.set("view engine", "hbs");
+
+app.use("/assets", express.static("public"));
+app.use(parser.urlencoded({extended: true}));
+
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialsDir:    "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main"
+}));
+
+// Root ALL!
+app.get("/", function(req, res) {
+  Restaurant.find({}).then((restaurants) => {
+    res.render("restaurants-index", {
+      restaurants
+    });
+  });
+})
+
+// NEW
+app.post("/", function(req, res) {
+  Restaurant.create(req.body.restaurant).then(function(restaurant) {
+    res.redirect("/${restaurant.name}");
+  });
+});
+
+// SHOW
+app.get("/:name", function(req, res) {
+  Restaurant.findOne({name: req.params.name}).then(function() {
+    res.redirect("show", {restaurant});
+  });
+});
+
+// UPDATE
+app.post("/:name", function(req, res) {
+  Restaurant.findOneAndUpdate({name: req.params.name}, req.body.restaurant, {new: true}).then(function(restaurant) {
+    res.redirect("/${restaurant.name}");
+    });
+});
+
+// DELETE
+app.post("/:name/delete", function(req, res) {
+  Restaurant.findOneAndRemove
+})
+
+app.listen(app.get("port"), function(){
+  console.log("You are is Working and Healthy");
+});
