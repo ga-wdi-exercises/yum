4f634fbc7544ee24c49855f8fe499d96f6b850e9
Taylor Scott <TaylorScott@Taylors-MBP.fios-router.home>
2017-03-22 22:07:38 -0400

    done?

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,19 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema,
+ ObjectId = Schema.ObjectId
+
+
+var RestaurantSchema = mongoose.Schema({
+  name: String,
+  address: { street: String, state: String, zipcode: Number },
+  yelp: String
+});
+
+var RestaurantModel = mongoose.model('Restaurant', RestaurantSchema);
+
+module.exports = {
+  Restaurant: RestaurantModel
+};
