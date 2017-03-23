b7f52ea3141492b648c4bae4e30bb2c25152eeb8
Natasha Clark <Natasharclark@gmail.com>
2017-03-23 09:01:56 -0400

    almost done with mongoose

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
-var mongoose = require('mongoose');
-var Schema = require("./db/schema.js");
+var mongoose = require('mongoose')
+
+var Schema = require("./db/schema.js")
+
+var Restaurant = Schema.Restaurant
+var Menu = Schema.Menu
+
+
+// find by name function
+// function finByName(restaurant) {
+//   Restaurant.findOne(name: restaurant, function() {
+//     if (err){
+//       console.log(err)
+//     }else {
+//       console.log(result)
+//     }
+//   })
+// }
+// Restaurant.findOne(name, callback)
+
+//find by zip
+// Restaurant.findBy(zip, callback)
+
+
+var resturantsController = {
+  index() {
+    Restaurant.find({}, (err, students) => {
+      console.log(students)
+    })
+  },
+  show(resturant){
+    Restaurant.findOne({name: name}, (err, resturant) => {
+      console.log(resturant)
+    })
+  }
+}
+resturantsController.index()
+// resturantsController.show({name: "savrajs"})
