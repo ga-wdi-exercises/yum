a79bc14abbab6fd0be2a253ccefd20f9a5ded50d
Mike Nabil <maikel_nabil@yahoo.com>
2017-03-22 20:05:47 -0400

    added index and show functionality

--- index.js
+++ index.js
@@ -33,59 +33,86 @@ app.listen(app.get("port"), function(){
 });
 
 //Routes
-
-
-
-
-
-
-
-
-
-
-
-//----------------------------------------------
-//Part 1
-
-//Create a new restaurant.
-Restaurant.create({name: "Mike's Backery"}, (err, restaurant) => {
-  // console.log(restaurant);
-})
-
-//Write a function or method that finds a restaurant by name.
-Restaurant.findOne({ name: "Rosa Mixicana"}, (err, restaurant) => {
-  // console.log(restaurant);
-});
-
-//Write a function or method that finds all restaurants by zipCode.
-function findByZip(zipcode){
-  results = [];
-  Restaurant.find({}, function(err, docs){
-    docs.forEach(function(rest){
-      if(rest.address.zipcode === zipcode){
-        results.push(rest);
-      }
+app.get("/", function(req, res){
+  Restaurant.find({}).then( (restaurants) => {
+    console.log(restaurants)
+    res.render("index", {
+      restaurants: restaurants
     });
-    // console.log(results);
-  });
-}
-findByZip(20011)
-
-//Create a function that updates a restaurant.
-Restaurant.findOneAndUpdate({ name: "Rosa Mixicana"}, {yelpUrl: "www.yelp.com/rosa"}, {new: true}, (err, restaurant) => {
-  // console.log(restaurant)
+  })
 });
 
-//Write a function or method that deletes a restaurant.
-Restaurant.findOneAndRemove({name: "Rosa Mixicana"}, function(err, docs){
-  if(err){
-    console.log(err);
-  }
-  else{
-    // console.log(" Restaurant was deleted!");
-  }
+app.post("/", function(req, res){
+    Restaurant.create(req.body.restaurant).then( (restaurant) => {
+      res.redirect("/" + restaurant.name)
+    })
 });
 
+app.get("/:name", function(req, res){
+  Restaurant.findOne({name: req.params.name}).then( (restaurant) => {
+    console.log(restaurant)
+    res.render("show", {
+      restaurant: restaurant
+    });
+  })
+});
+
+app.post("/:name", function(req, res){
+    Restaurant.findOneAndUpdate({name: req.params.name}, req.body.restaurant, {new: true}).then( (restaurant) => {
+      res.redirect("/" + restaurant.name)
+    })
+});
+
+app.post("/:name/delete", function(req, res){
+    Restaurant.findOneAndRemove({name: req.params.name}).then( () => {
+      res.redirect("/")
+    })
+});
+
+
+
+// //----------------------------------------------
+// //Part 1
+//
+// //Create a new restaurant.
+// Restaurant.create({name: "Mike's Backery"}, (err, restaurant) => {
+//   // console.log(restaurant);
+// })
+//
+// //Write a function or method that finds a restaurant by name.
+// Restaurant.findOne({ name: "Rosa Mixicana"}, (err, restaurant) => {
+//   // console.log(restaurant);
+// });
+//
+// //Write a function or method that finds all restaurants by zipCode.
+// function findByZip(zipcode){
+//   results = [];
+//   Restaurant.find({}, function(err, docs){
+//     docs.forEach(function(rest){
+//       if(rest.address.zipcode === zipcode){
+//         results.push(rest);
+//       }
+//     });
+//     // console.log(results);
+//   });
+// }
+// findByZip(20011)
+//
+// //Create a function that updates a restaurant.
+// Restaurant.findOneAndUpdate({ name: "Rosa Mixicana"}, {yelpUrl: "www.yelp.com/rosa"}, {new: true}, (err, restaurant) => {
+//   // console.log(restaurant)
+// });
+//
+// //Write a function or method that deletes a restaurant.
+// Restaurant.findOneAndRemove({name: "Rosa Mixicana"}, function(err, docs){
+//   if(err){
+//     console.log(err);
+//   }
+//   else{
+//     // console.log(" Restaurant was deleted!");
+//   }
+// });
+//
 // //Write methods to add embedded menu item documents for a restaurant of your choosing.
 // Restaurant.findOne({name: "Busboys and Poets"}, function(err, restaurant){
 //   restaurant.items.push(new Menuitem({title: "candy"}))
@@ -98,19 +125,19 @@ Restaurant.findOneAndRemove({name: "Rosa Mixicana"}, function(err, docs){
 //     }
 //   })
 // });
-
-//Write methods to remove embedded menu item documents for a restaurant of your choosing.
-function removeItem(resturant, item){
-  Restaurant.findOneAndUpdate({name: resturant}, {
-    $pull: { items: {title: item} }
-  },
-  {new: true}, function(err, docs){
-    if(err){
-      console.log(err);
-    }
-    else{
-      console.log(docs);
-    }
-  });
-}
-removeItem("Busboys and Poets", "Beef")
+//
+// //Write methods to remove embedded menu item documents for a restaurant of your choosing.
+// function removeItem(resturant, item){
+//   Restaurant.findOneAndUpdate({name: resturant}, {
+//     $pull: { items: {title: item} }
+//   },
+//   {new: true}, function(err, docs){
+//     if(err){
+//       console.log(err);
+//     }
+//     else{
+//       console.log(docs);
+//     }
+//   });
+// }
+// removeItem("Busboys and Poets", "Beef")
