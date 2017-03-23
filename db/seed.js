ff0ef06ac8b83b574acb23fb60b6de7b0da46655
Malcom Johnson <malcomjohnson@malcoms-MacBook-Pro.local>
2017-03-23 00:04:55 -0400

    commit

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,46 @@
+const mongoose = require('./connection')
+
+const MenuItem = mongoose.model('MenuItem')
+const Restaurant = mongoose.model('Restaurant')
+
+MenuItem.remove({}).then(()=>{
+  MenuItem.collection.insert().then(()=>{
+    process.exit()
+})
+})
+
+Restaurant.remove({}).then(()=>{
+  Restaurant.collection.insert().then(()=>{
+    process.exit()
+})
+})
+
+
 var Schema = require("../db/schema.js");
+
+var MenuItem = Schema.MenuItem
+var Restaurant = Schema.Restaurant
+
+
+var restaurant1 = new Restaurant({name: "District Taco", address: {street: "1309 F St NW", zipcode: 20004}, yelpUrl: "https://www.yelp.com/biz/district-taco-washington-6"})
+var restaurant2 = new Restaurant({name: "KFC", address: {street: "220 Riggs Rd NE", zipcode: 20011}, yelpUrl: "https://www.yelp.com/biz/kfc-washington-6"})
+var restaurant3 = new Restaurant({name: "Federalist Pig", address: {street: "1654 Columbia Rd NW", zipcode: 20009}, yelpUrl: "https://www.yelp.com/biz/federalist-pig-washington-3?osq=Food"})
+
+var item1 = new MenuItem({title: "Taco"})
+var item2 = new MenuItem({title: "Chicken"})
+var item3 = new MenuItem({title: "Burger"})
+
+var restaurants = [restaurant1, restaurant2, restaurant3]
+var menuItems = [item1, item2, item3]
+
+restaurants.forEach((restaurant, i) => {
+  restaurant.items.push(menuItems[i], menuItems[i + 1])
+  restaurant.save((err, restaurant) => {
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(restaurant)
+    }
+  })
+})
--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-var mongoose = require('mongoose');
-var Schema = require("./db/schema.js");
+const express = require('express')
+const hbs = require('hbs')
+const mongoose = require('./db/connection')
+const parser = require('body-parser')
+
+const app = express()
+
+const MenuItem = mongoose.model("MenuItem")
+const Restaurant = mongoose.model("Restaurant")
+
+app.set("port", process.env.PORT || 3000)
+app.set("view engine", "hbs")
+app.engine('.hbs', hbs({
+  extname: ".hbs",
+  partialsDir: "views/",
+  layoutsDir: "views/",
+  defaultLayout: "layout-main"
+}))
+
+app.use("/assets", express.static("public"))
+app.use(parser.urlencoded({extended: true}))
+
+app.get('/restaurants', function(req, res){
+  Restaurant.find({}).then((restaurants)=>{
+    res.render("index", {
+      restaurants: restaurants
+    })
+  })
+})
