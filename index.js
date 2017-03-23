af2d3745da87c2240a453dacd5b5754af1710e6d
Andy Flickinger <atflick@gmail.com>
2017-03-22 22:31:59 -0400

    query functions

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+// find restaurant
+function findRestaurant(name) {
+  Restaurant.findOne({name: name}).then(res) => {
+    return res
+  })
+}
+// find restaurant by zip
+function findZips(zip) {
+  Restaurant.find({zipcode: zip}).then(res) => {
+    return res
+  }
+}
+// update restaurant
+function update(current, updated) {
+  Restaurant.findOneAndUpdate(current, updated).then(res) => {
+    return res
+  }
+}
+// delete restaurant
+function delete(restaurant) {
+  Restaurant.findOneAndRemove(restaurant).then() => {
+    return {success: true}
+  }
+}
+// add item to restaurant
+function addItem(restaurant, item) {
+  let restaurant = findRestaurant(restaurant)
+  restaurant.items.push(item)
+  return restaurant
+}
+// remove item from restaurant
+function removeItem(restaurant, item) {
+  let restaurant = findRestaurant(restaurant)
+  let index = restaurant.items.indexOf(item)
+  restaurant.items.splice(index, 1)
+  return restaurant
+}
