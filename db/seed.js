61a6c456c61a01378c60d79ad08abaf21964c268
Aaron <andrews_aaron@bah.com>
2017-03-22 22:46:34 -0400

    part 1

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,14 @@
 var Schema = require("../db/schema.js");
+var Restaurant = Schema.Restaurant;
+var MenuItem = Schema.MenuItem;
+
+Restaurant.remove({}, err =>{
+  if(err){
+    console.log(err);
+  }
+});
+MenuItem.remove({}, err =>{
+  if(err){
+    console.log(err);
+  }
+});
--- index.js
+++ index.js
@@ -1,2 +1,74 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var RestaurantModel = Schema.RestaurantModel;
+var MenuItemModel = Schema.MenuItemModel;
+
+var RestaurantController = {
+  function(){
+    RestaurantModel.find({}, function(err, docs){
+      console.log(docs);
+      return docs;
+    });
+  }
+
+function findName(req){
+  RestaurantModel.fineOne({"name": req.name}, function(err, docs){
+    console.log(docs);
+    return docs;
+  });
+},
+
+function findZip(req){
+  Resturant.find({address: {zipcode: req.zipcode}}, (err, restaurant) =>{
+    if(err){
+      console.log(err);
+    } else {
+      console.log(docs);
+    }
+  })
+}
+function update(req, update){
+  RestaurantModel.findOneAndUpdate(req, update, {new: true},function(err, docs){
+    if(err){
+      console.log(err);
+    } else{
+      console.log(docs);
+    }
+  });
+},
+function delete(req){
+  RestaurantModel.findOneAndRemove(req, function(err, docs){
+    if(err){
+      console.log(err);
+    } else{
+      console.log(docs);
+    }
+  });
+},
+function deleteAll(req){
+  RestaurantModel.find({}, function(err, docs){
+    if (err) {
+      console.log(err);
+    } else {
+      docs.forEach(function(restaurant){
+        if (restaurant.name === req.name){
+          restaurant.remove().then(function(results){
+            console.log(results);
+          });
+        }
+      });
+    }
+  });
+},
+function removeMenuItem(req, project){
+  RestaurantModel.findOneAndUpdate(req, {
+    $pull: { menuItems: {body: project.body}}
+  }, {new: true}, function(err, docs){
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(docs);
+    }
+  });
+}
+};
