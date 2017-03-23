5c1dfee434b1c3c69687a7c1a3570620d13b2c8e
Lisa Anne Burr <burr_lisa@bah.com>
2017-03-22 22:01:32 -0400

    initial setup and seed

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,34 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/yum');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/yum')
+
+var db = mongoose.connection
+
+db.on('error', err => {
+  console.log(err)
+})
+
+db.once('open', () => {
+  console.log("Mongo connected!")
+})
+
+var Schema = mongoose.Schema
+
+var ItemSchema = new Schema({
+  title: String,
+})
+
+var RestaurantSchema = new Schema({
+  name: String,
+  street: String,
+  zipcode: Number,
+  yelpUrl: String,
+  items: [ {type: Schema.ObjectId, ref: "Item"}]
+})
+
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema)
+var Item = mongoose.model("Item", ItemSchema)
+
+module.exports = {
+  Restaurant: Restaurant,
+  Item: Item
+}
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,40 @@
 var Schema = require("../db/schema.js");
+
+var Restaurant = Schema.Restaurant
+var Item = Schema.Item
+
+Restaurant.remove({}, err => {
+  if(err){
+    console.log(err);
+  }
+})
+
+Item.remove({}, err => {
+  if(err){
+    console.log(err);
+  }
+})
+
+var mcdonalds = new Restaurant({name: "McDonalds"})
+
+var mcdouble = new Item({title: "McDouble"})
+var bigmac = new Item({title: "Big Mac"})
+var mcflurry = new Item({title: "McFlurry"})
+var qpcheese = new Item({title: "Quarter Pounder with Cheese"})
+var fries = new Item({title: "French Fries"})
+var applepie = new Item({title: "Apple Pie"})
+var nuggets = new Item({title: "Chicken McNuggets"})
+
+var restaurants = [mcdonalds]
+var items = [mcdouble, bigmac, mcflurry, qpcheese, fries, applepie, nuggets]
+
+for(var i=0; i < restaurants.length; i++){
+  restaurants[i].items.push(items[i], items[i+1])
+  restaurants[i].save((err, restaurant) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(restaurant);
+    }
+  })
+};
