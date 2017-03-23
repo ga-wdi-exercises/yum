a6ad4f60559a6d80b624b66eab80424d7dca8dc0
KEVIN KELLER <keller_kevin@bah.com>
2017-03-22 22:57:39 -0400

    schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,21 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+const mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId
+
+var menuSchema = new Schema {
+  title: String
+}
+
+var restaurantSchema = new Schema {
+  name: String,
+  address: {
+    zipCode: Number,
+    street: String
+  },
+  yelpUrl: String,
+  item: [menuSchema]
+}
