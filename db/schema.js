47a8313d0cd9928daf01847d6953caec8e56d54d
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-03-22 20:11:48 -0400

    schema and models

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,31 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+
+var RestaurantSchema = new mongoose.Schema(
+  {
+    name: String,
+    address:{
+      street: String,
+      zipcode: Number
+    },
+    yelpURL: String
+  }
+)
+
+
+//
+// var Menu_itemSchema = new mongoose.Schema({
+//     title: String
+//   }
+// )
+
+mongoose.model("Restaurant", RestaurantSchema);
+// mongoose.model("Menu_item", Menu_itemSchema)
+//
+// module.exports = {
+//   Restaurant: Restaurant,
+//   // Menu_item: Menu_itemModel
+// }
