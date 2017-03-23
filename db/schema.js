7f0aa9fd2bffee161a92d9c5412727949664686e
Samuel Clements <sdkclements@gmail.com>
2017-03-23 11:08:58 -0400

    added initial schema

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,23 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+let RestaurantSchema = new mongoose.Schema(
+{
+	name: String,
+	address: {
+		street: String,
+		zipcode: Number
+	},
+	yelpUrl: String,
+	items: [MenuItems]
+})
+
+let MenuItemsSchema = new mongoose.Schema(
+{
+	title: String
+})
+
+mongoose.model("Restaurant", RestaurantSchema)
+mongoose.model("MenuItems", MenuItemsSchema)
+
+module.exports = mongoose
