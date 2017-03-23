e2114a837bd6ddf2fa19b2a204d2c7dc75a309e2
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-03-23 10:32:41 -0400

    updating

--- index.js
+++ index.js
@@ -15,11 +15,39 @@ app.set("port", process.env.PORT || 3001);
 app.set("view engine", "hbs");
 
 app.use("/assets", "express.static("public"));
-app.use(parser.urlencoded({extended:true});
+app.use(parser.urlencoded({extended:true}));
 
 app.engine(".hbs", hbs({
   extname: ".hbs",
-  partialsDir ".views/",
-  layoutsDir ".views/",
-  defaultLayout "layout-main",
+  partialsDir: ".views/",
+  layoutsDir: ".views/",
+  defaultLayout: "layout-main"
 }));
+
+app.get("/", function(req, res) {
+  Restaurant.find({}).then((restaurants) => {
+    res.render("restaurants-index", {
+      restaurants
+    });
+  });
+})
+
+app.post("/", function(req, res)) {
+  Restaurant.create(req.body.restaurant).then(function(restaurant){
+    res.redirect("/$restaurant.name");
+  });
+});
+
+app.get(":name", fucntion(req, res) {
+  Restaurant.findOne({name: req.params.name}).then(function() {
+    res.redirect("show", {restaurant});
+  });
+});
+
+app.post(":name", fucntion(req, res) {
+  Restaurant.findOne({name: req.params.name}, req.body.restaurant, {new: true}).then(function(restaurant){
+    res.redirect("/${restaurant.name}");
+  });
+    res.redirect("show", {restaurant.name}");
+  });
+});
