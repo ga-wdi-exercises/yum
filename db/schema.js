d312ca3bd9bc945fe6aed1c460913e185bc31806
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-05-20 16:59:23 -0400

    added schema.js data

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,34 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
+
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+
+db.on('error', err => {
+  console.log(err);
+});
+
+db.once('open', () => {
+  console.log("Connected MongoDB!");
+});
+
+var Schema = mongoose.Schema
+var ObjectId = Schema.ObjectId
+
+var MenuItemsSchema = new Schema({
+  title: String,
+})
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: {street: String, zipcode: Number},
+  yelpUrl: String,
+  items = [MenuItems]
+});
+
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+var MenuItems = mongoose.model("MenuItems", MenuItemsSchema);
+
+module.exports = {
+  Restaurant: Restaurant,
+  MenuItems: MenuItems
+}
