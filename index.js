0b75ca6ad108aed4f5d6e8fbf30c0c2b24f86eb3
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-05-22 19:16:13 -0400

    pre-setup and working on db

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var mongoose = require('mongoose');
-var Schema = require("./db/schema.js");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(4000, () => {
+  console.log("app listening on port 4000");
+});
