ac48b351730a1ed982b7c58c815342c30630a5fb
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-05-23 14:32:47 -0400

    started Express and connections.js

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+var RestaurantSchema = new mongoose.Schema(
+  {
+    name: String,
+    address: {street: String, zipcode: Number},
+    yelpUrl: String,
+    items: [ItemSchema]
+  }
+);
+
+mongoose.model("Restaurant", RestaurantSchema);
+mongoose.connect("mongodb://localhost/yum");
+
+var seedData = require("./seeds.json");
+
+module.exports = mongoose;
