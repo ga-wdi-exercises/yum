06f946ea5836a36f161d90985b8f8620fb6b2d56
Erin <erinedawson@gmail.com>
2017-05-23 18:10:34 -0400

    create schemas and models

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,20 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+
+var RestaurantSchema = new Schema ({
+  name: String,
+  address: {'street': String, 'zipcode': Number},
+  yelpUrl: String,
+  items: [MenuItemsSchema]
+});
+
+var MenuItemsSchema = new Schema ({
+  title: String
+})
+
+
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+var MenuItem = mongoose.model("MenuItem", MenuItemsSchema);
