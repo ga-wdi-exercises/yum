f087f108b899322da85030ba879f41f371ef3fee
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-05-23 14:12:50 -0400

    almost part 1

---
+++ controllers/restaurantsController.js
@@ -0,0 +1,42 @@
+var Schema = require("../db/schema.js");
+var Restaurant = Schema.Restaurant;
+var MenuItem = Schema.MenuItem;
+
+var restaurantsController = {
+    index(){
+        Restaurant.find({}, (err, restaurants) => {
+            console.log(restaurants);
+        });
+    },
+    show(req){
+        Restaurant.findOne({name: req.name}, (err, restaurant) => {
+            console.log(restaurant);
+        });
+    },
+    
+    update: function(req, update){
+        RestaurantModel.findOneAndUpdate(req, update, {new: true}, function(err, docs){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(docs);
+            }
+        });
+    },
+    destroy: function(req){
+        RestaurantModel.findOneAndRemove(req, function(err, docs){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(docs);
+            }
+        });
+    }
+};
+
+restaurantsController.destroy({name: "Ozio Rooftop"});
+
+restaurantsController.index();
+restaurantsController.show({name: "Ozio Rooftop"})
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,49 @@
 var Schema = require("../db/schema.js");
+
+var Restaurant = Schema.Restaurant
+var MenuItem = Schema.MenuItem
+
+Restaurant.remove({}, err => {
+  if(err){
+    console.log(err)
+  }
+});
+
+MenuItem.remove({}, err => {
+  if(err){
+    console.log(err)
+  }
+});
+
+var alero = new Restaurant({ name: "Alero Restaurant", address: { street: "1300 U St NW", zipcode: 20001, }, yelp: "https://www.yelp.com/biz/alero-restaurant-washington-2"})
+
+var ozio = new Restaurant({ name: "Ozio Rooftop", address: { street: "944 Conneticut Ave", zipcode: 20001 }, yelp: "https://www.yelp.com/biz/ozio-washington"})
+
+var mandu = new Restaurant({ name: "Mandu Korean", address: { street: "453 K St", zipcode: 20001}, yelp: "https://www.yelp.com/biz/mandu-washington-2"})
+
+var alba = new Restaurant({ name: "Alba Osteria", address: { street: "300 4th St NW", zipcode: 20001 }, yelp: "https://www.yelp.com/biz/alba-osteria-washington"})
+
+var wise = new Restaurant({ name: "Wise Guys", address: { street: "1120 4th St NW", zipcode: 22209}, yelp: "https://www.yelp.com/biz/wiseguy-pizza-washington-3?osq=wise+guys"})
+
+var item1 = new MenuItem({title: "lasagna"})
+var item2 = new MenuItem({title: "tacos"})
+var item3 = new MenuItem({title: "pizza"})
+var item4 = new MenuItem({title: "omelette"})
+var item5 = new MenuItem({title: "salad"})
+var item6 = new MenuItem({title: "udon"})
+var item7 = new MenuItem({title: "pancakes"})
+
+var menuItems = [item1, item2, item3, item4, item5, item6, item7]
+var restaurants = [alero, ozio, mandu, alba, wise]
+
+restaurants.forEach(function(restaurant, i){
+    restaurant.items.push(menuItems[i], menuItems[i+3]);
+    restaurant.save(function(err, docs){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log(docs);
+      }
+    });
+  });
