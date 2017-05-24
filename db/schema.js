72d2494a34a7e2e13210e498363c7d7ff14f34b8
Andres Arango <andresarangov17@gmail.com>
2017-05-24 17:35:14 -0400

    create schema and models for restaurant and menu

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,20 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+
+var MenuSchema = new Schema({
+  title: String,
+});
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: String,
+  zipcode: Number,
+  yelpUrl: String,
+  items: [MenuSchema]
+});
+
+var Menu = mongoose.model("Menu", MenuSchema);
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
