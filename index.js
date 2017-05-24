6a62c574751ec7a41bbe6e78ea8a072ccda2403e
Jonathan Wright <jonwright.va@gmail.com>
2017-05-23 22:27:22 -0400

    answers questions 4 and 5 in part 1

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+
+let Restaurant = Schema.RestaurantModel
+let MenuItem = Schema.MenuItemModel
+
+let number4 = Restaurant.findOne({name: "Pizza Hut"}, (err, restaurant) => {
+	console.log("number 4: " + restaurant)
+})
+
+let number5 = Restaurant.find( {"address.zipcode": 43953  }, (err, restaurants) => {
+	console.log("number 5: " + restaurants)
+})
