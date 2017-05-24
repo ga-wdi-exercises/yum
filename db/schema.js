57fa330d376b15ba2b30b5b0ab71e92117b2a319
Curtis Ostle <curtisostle@gmail.com>
2017-05-23 20:40:36 -0400

    initial commit

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,9 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+var Schema = mongoose.Schema
+
+var RestaurantSchema = new Schema ({})
+
+var ItemSchema = new Schema ({})
