f002acbc105970366b18bf8f8a7006cc58a0eb72
Jonathan Wright <jonwright.va@gmail.com>
2017-05-23 22:01:49 -0400

    adds schema and seeds, db working

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,22 @@
-var Schema = require("../db/schema.js");
+let mongoose = require("mongoose")
+let Schema = require('../db/schema.js')
+
+let MenuItemModel = Schema.MenuItemModel
+let RestaurantModel = Schema.RestaurantModel
+
+let pizza = new MenuItemModel({name: "Pizza"})
+let breadSticks = new MenuItemModel({name: "BreadSticks"})
+
+let pizzaHut = new RestaurantModel({
+	name: "Pizza Hut",
+	address: {
+		street: "2343 BlahBlah St.",
+		zipcode: 43953
+	},
+	yelpUrl: "www.yelp.com",
+	item: [pizza, breadSticks]
+})
+
+pizza.save()
+breadSticks.save()
+pizzaHut.save()
