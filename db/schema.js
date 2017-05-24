995bf2590682dd7c5fdcad1c74d8a7f60dee5b4c
Caroline Chen <carolinecaroline@gmail.com>
2017-05-23 21:46:09 -0400

    seed database

--- db/schema.js
+++ db/schema.js
@@ -1,4 +1,32 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", function(err) {
+	console.log(err);
+});
+
+db.once("open", function() {
+	console.log("database has been connected!");
+});
+
+var Schema = mongoose.Schema
+
+var MenuItemSchema = new Schema({
+	title: String,
+});
+
+var RestaurantSchema = new Schema({
+	name: String,
+	address: {street: String, zipcode: Number},
+	yelpUrl: String,
+	items: [MenuItemSchema]
+});
+
+
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+var MenuItem = mongoose.model("MenuItem", MenuItemSchema)
+
+module.exports = {
+	Restaurant: Restaurant,
+	MenuItem: MenuItem
+};
