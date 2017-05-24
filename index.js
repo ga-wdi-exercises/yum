077eb112b888d41ec9c0c445471ea21de543318d
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-05-24 01:44:12 -0400

    Final commits

--- index.js
+++ index.js
@@ -1,2 +1,82 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var MenuItem = Schema.MenuItem;
+var Restaurant = Schema.Restaurant;
+
+Restaurant.create({ name: 'Lookie Cookies!', "address.zipcode": 20912 }, function (err, restaurant) {
+  if (err) return handleError(err);
+  console.log("We created", restaurant.name, 'in', restaurant.address.zipcode);
+});
+
+function findByName(restaurant){
+  Restaurant.findOne({name: restaurant}, function(err, result){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(result);
+    }
+  });
+}
+
+function findByZip(zipcode){
+  results = [];
+  Restaurant.find({}, function(err, docs){
+    docs.forEach(function(rest){
+      if(rest.address.zipcode === zipcode){
+        results.push(rest);
+      }
+    });
+    console.log(results);
+  });
+}
+
+function update(resturant, update){
+  Restaurant.findOneAndUpdate({name: resturant}, {name: update}, {new: true}, function(err, docs){
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(docs);
+    }
+  });
+}
+
+function destroy(resturant){
+  Restaurant.findOneAndRemove({name: resturant}, function(err, docs){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(docs + " was deleted!");
+    }
+  });
+}
+
+function addItem(resturant, item){
+  Restaurant.findOne({name: resturant}, function(err, docs){
+    docs.items.push(new MenuItem({title: item}))
+    docs.save(function(err, results){
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log(results);
+      }
+    })
+  });
+}
+
+function removeItem(resturant, item){
+  Restaurant.findOneAndUpdate({name: resturant}, {
+    $pull: { items: {title: item} }
+  },
+  {new: true}, function(err, docs){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(docs);
+    }
+  });
+}
