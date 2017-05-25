43102a006ca2558a2ec833612fb028d1079e36bd
Jeong Hoon Koo <junghoone17@gmail.com>
2017-05-24 23:07:18 -0400

    Schema set up

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,27 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+let schema = mongoose.Schema;
+
+let MenuItemSchema = new Schema ({
+  title: String
+});
+
+let RestaurantSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [MenuItemSchema]
+});
+
+let Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+let MenuItem = mongoose.model("MenuItem", MenuItemSchema);
+
+module.exports = {
+  RestaurantModel: Restaurant,
+  MenuItemModel: MenuItem
+}
