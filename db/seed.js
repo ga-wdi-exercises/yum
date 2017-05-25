883c940879538d9faf49bca275fd82ab35df374c
Luisa Gonzalez <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-05-25 00:38:37 -0400

    seeds update

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,29 @@
 var Schema = require("../db/schema.js");
+var Restaurant = Schema.Restaurant;
+var MenuItem = Schema.MenuItem;
+
+
+
+Restaurant.remove({}, function(err){
+  console.log(err)
+})
+MenuItem.remove({}, function(err){
+  console.log(err)
+})
+
+
+
+  resturant1 = new Restaurant({ name: "I-Thai Restaurant & Sushi Bar", address: { street: "3003 M St NW", zipcode: 20007, }, yelp: "https://www.yelp.com/biz/i-thai-restaurant-and-sushi-bar-washington-2"})
+  resturant2 = new Restaurant({ name: "Tryst", address: { street: "2459 18th St NW", zipcode: 20009 }, yelp: "https://www.yelp.com/biz/tryst-washington"})
+  resturant3 = new Restaurant({ name: "La Puerta Verde", address: { street: "2001 Fenwick St NE", zipcode: 20002}, yelp: "https://www.yelp.com/biz/la-puerta-verde-washington"})
+  resturant4 = new Restaurant({ name: "Bar Pilar", address: { street: "1833 14th St NW", zipcode: 20009 }, yelp: "https://www.yelp.com/biz/bar-pilar-washington"})
+  resturant5 = new Restaurant({ name: "Patisserie Poupon", address: { street: "1645 Wisconsin Ave NW", zipcode: 20007}, yelp: "https://www.yelp.com/biz/patisserie-poupon-washington"})
+
+  var item1 = new MenuItem({ title: "Pineapple Fried Rice"})
+  var item2 = new MenuItem({ title: "Green Curry"})
+  var item3 = new MenuItem({ title: "Fruit tart"})
+  var item4 = new MenuItem({ title: "Ceviche de Huachinango"})
+  var item5 = new MenuItem({ title: "Deviled duck eggs"})
+  var item6 = new MenuItem({ title: "Smoked Salmon Salad"})
+
+  
