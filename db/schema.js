3866837dd861ffe7d76368aa40a768defb8b3ac8
Erin <erinedawson@gmail.com>
2017-05-24 22:38:50 -0400

    create new restaurant and menu item

--- db/schema.js
+++ db/schema.js
@@ -24,3 +24,17 @@ module.exports = {
   Restaurant: Restaurant,
   MenuItem: MenuItem
 };
+
+var pica = new Restaurant({name: "Pica Taco", address:{street: "1629 Columbia Rd NW", zipcode: 20009}, yelpUrl: "https://www.yelp.com/biz/pica-taco-washington"})
+var taco = new MenuItem({title: "Tacos al Pastor"})
+
+pica.items.push(taco)
+
+pica.save((err, restaurant) => {
+  if(err){
+    console.log(err);
+  }
+  else{
+    console.log(restaurant);
+  }
+});
