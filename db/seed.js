51bedabf8fa12d2b9672c6b8914b843e632ec34c
Tarik Kazanci <tarikkzc@gmail.com>
2017-07-03 23:41:53 -0400

    created schema, models, seed data and created some restaurants

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,20 @@
 var Schema = require("../db/schema.js");
+
+var Restaurant = Schema.Restaurant
+var MenuItem = Schema.MenuItem
+
+Restaurant.remove({}, err => {
+  if(err){
+    console.log(err)
+  }
+});
+
+MenuItem.remove({}, err => {
+  if(err){
+    console.log(err)
+  }
+});
+
+var kazan = new Restaurant({ name: "Kazan", address: {street: "6813 Redmond Dr", zipcode: 22101}, yelp: https: "https://www.yelp.com/biz/kazan-restaurant-mclean" })
+var bozellis = new Restaurant({ name: "Bozelli's", address: {street: "1025 Vermont Ave NW", zipcode: 20005}, yelp: https: "https://www.yelp.com/biz/bozzellis-washington" })
+var vapiano = new Restaurant({ name: "Vapiano", address: {street: "1800 M St NW", zipcode: 20036}, yelp: https: "https://www.yelp.com/biz/vapiano-washington-2" })
