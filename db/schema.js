9c9dc436d2e3cead090e780bf4ae8b9756b2ffb0
Fitssum Haile <fitsum.wolde@gmail.com>
2017-07-05 19:09:30 -0400

    added seeds data for restaurant and menu items

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,19 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("database has been connected!");
+});
+
+var Schema = mongoose.Schema
+
+var RestaurantSchema = new Schema({
+  name: String,
+  address: { street: String, state: String, zipcode: Number},
+  yelp: String,
+  items: [MenuSchema]
+});
+
+var MenuSchema = new Schema({
+  title: String,
+});
