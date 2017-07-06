d71a1fa221a0ef542b2a8a70d29bf12ce3261258
Fitssum Haile <fitsum.wolde@gmail.com>
2017-07-06 03:42:21 -0400

    part one completed

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var Schema = require("./db/schema.js");
 var Restaurant = Schema.Restaurant;
 var Menu = Schema.Menu;
 
+Restaurant.create({ name: 'Langano', "address.zipcode": 20902 }, function (err, restaurant) {
+  if (err) return handleError(err);
+  console.log("New restaurant created", restaurant.name, 'in', restaurant.address.zipcode);
+});
+
 function findByName(restaurant){
   Restaurant.findOne({name: restaurant}, function(err, result){
     if(err){
@@ -24,5 +29,55 @@ function findByZip(zipcode){
       }
     });
     console.log(results);
+  });
+}
+
+function update(resturant, update){
+  Restaurant.findOneAndUpdate({name: resturant}, {name: update}, {new: true}, function(err, docs){
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log(docs);
+    }
+  });
+}
+
+function destroy(resturant){
+  Restaurant.findOneAndRemove({name: resturant}, function(err, docs){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(docs + " was deleted!");
+    }
+  });
+}
+
+function addItem(resturant, item){
+  Restaurant.findOne({name: resturant}, function(err, docs){
+    docs.items.push(new Menu({title: item}))
+    docs.save(function(err, results){
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log(results);
+      }
+    })
+  });
+}
+
+function removeItem(resturant, item){
+  Restaurant.findOneAndUpdate({name: resturant}, {
+    $pull: { items: {title: item} }
+  },
+  {new: true}, function(err, docs){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(docs);
+    }
   });
 }
