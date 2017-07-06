4d0ba707e47c580ff073ac38efe50a0a71407693
Curry Lee <ej20curry@gmail.com>
2017-07-06 09:12:00 -0400

    I could make these work

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+var Restaurant = Schema.Restaurant;
+var MenuItem = Schema.MenuItem;
+
+
+Restaurant.create({ name: 'Jeff\'s Jerk Hut', "address.zipcode": 20151 }, function (err, restaurant) {
+  if (err) return handleError(err);
+  console.log("You made ", restaurant.name, ' located in ', restaurant.address.zipcode);
+});
+
+
+function findName(restaurant){
+  Restaurant.findOne({name: restaurant}, function(err, result){
+    if(err){
+      
+      console.log(err);
+    }
+    else{
+
+      console.log(result);
+
+    }
+  });
+}
+
+
+function destroy(resturant){
+  Restaurant.findOneAndRemove({name: resturant}, function(err, docs){
+
+    if(err){
+
+      console.log(err);
+    }
+
+    else{
+      console.log(docs + " has been removed. ");
+
+    }
+  });
+}
