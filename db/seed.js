9e113c46662a984effcadd2ce07d263989c7611f
Scott Pierce <scottpiercew@gmail.com>
2017-07-05 22:54:47 -0400

    schema and seed data made

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,12 @@
-var Schema = require("../db/schema.js");
+var Schema = require("./db/schema.js")
+var mongoose = require("./schema.js")
+var seedData = require("./seeds.json")
+
+
+var Restaurant = mongoose.model("Restaurant")
+
+Restaurant.remove({}).then(() => {
+  Restaurant.collection.insert(seedData).then(() => {
+    process.exit()
+  })
+})
