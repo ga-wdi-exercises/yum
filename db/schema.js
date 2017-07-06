a0e612c4ee8c3aab9c3e568e0a2adae93e1557d5
ANTWANA WADLINGTON <antwana.wadlington@gmail.com>
2017-07-06 08:48:08 -0400

    Initial Commit

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,23 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+
+var Schema = mongoose.Schema;
+
+var restaurantSchema = new Schema({
+  name: String,
+  address: Array[{
+    street: String,
+    zipcode: Number,
+  items: Array[{
+    title: String
+  }]
+  }]
+})
+
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema)
+
+// mongoose.connect("mongodb://localhost/whenpresident")
+
+module.exports = mongoose.model ()
--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,14 @@
 var Schema = require("../db/schema.js");
+var mongoose = require("./connection.js")
+
+// var seedData = require("./seeds.json")
+
+var Restaurant = mongoose.model("Restaurant")
+
+Restaurant.remove({}).then(() => {
+  Restaurant.collection.insert(seedData).then(() =>{
+    process.exit()
+  })
+})
+
+var benAndJerrys = new Restaurant({name: "Ben and Jerrys", address: [street: "123 Main Street", zipcode: 10978]})
