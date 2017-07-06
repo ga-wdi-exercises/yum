7988a179a8d029e4063c850fe1b1c67d6eeb37fa
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-07-05 22:24:44 -0400

    yum homework

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,27 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("database has been connected!");
+});
+
+var Schema = mongoose.Schema
+
+var MenuSchema = new Schema({
+  title: String
+})
+
+var RestaurantSchema =  new Schema({
+  name: String,
+  address: {street: String, state: String, zipcode: Number},
+  yelp: String,
+  items: [MenuSchema]
+})
+
+var Menu = mongoose.model("Menu", MenuSchema)
+var Restaurant = mongoose.model("Restaurant", RestaurantSchema)
+
+module.exports = {
+  Restaurant: Restaurant,
+  Menu: Menu
+}
