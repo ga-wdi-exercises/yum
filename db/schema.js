cdbf57e32adc1726ff5f76fd22445f44e616104c
Danielle Fricke <danielle.fricke11@gmail.com>
2017-07-05 21:32:27 -0400

    create restaurant and find

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,42 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+
+var Schema = mongoose.Schema,
+ ObjectId = Schema.ObjectId
+
+var MenuItemSchema = mongoose.Schema({
+  title: String
+});
+
+var RestaurantSchema = mongoose.Schema({
+  name: String,
+  address: { street: String, state: String, zipcode: Number },
+  yelp: String,
+  items: [MenuItemSchema]
+});
+
+var MenuItemModel = mongoose.model("MenuItemSchema", MenuItemSchema);
+var RestaurantModel = mongoose.model('Restaurant', RestaurantSchema);
+
+// var blueDuck = new Restaurant({name: "Blue Duck Tavern", address: {street: "2400 M St, NW", state: "Washington, DC", zipcode: 20037}, yelpURL:"http://www.yelp.com"})
+// var item8 = new MenuItem({item: "Chicken Tinga Tacos"})
+
+// blueDuck.restaurant.push(newRestaurant)
+//
+// blueDuck.save((err, author) => {
+//   console.log("what's up")
+//   if(err){
+//     console.error(err)
+//   } else {
+//     console.log(author)
+//     process.exit()
+//   }
+// })
+
+
+module.exports = {
+  Restaurant: RestaurantModel,
+  MenuItem: MenuItemModel
+};
--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 var mongoose = require('mongoose');
 var Schema = require("./db/schema.js");
+
+var Restaurant = Schema.Restaurant;
+var MenuItem = Schema.MenuItem;
+
+
+Restaurant.create({ name: 'Blue Duck Tavern', "address.zipcode": 20037, yelp: "https://www.yelp.com/biz/blue-duck-tavern-washington" }, function (err, restaurant) {
+  if (err) return handleError(err);
+  console.log("We created", restaurant.name, 'in', restaurant.address.zipcode);
+});
+
+function findByName(restaurant){
+  Restaurant.findOne({name: restaurant}, function(err, result){
+    if(err){
+      console.log(err);
+  }
+    else{
+      console.log(result);
+  }
+});
+}
