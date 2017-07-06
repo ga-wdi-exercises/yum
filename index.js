9f657379505960fd4688d341189d540dbf33e3b6
Danielle Fricke <danielle.fricke11@gmail.com>
2017-07-05 21:32:53 -0400

    additional functionality

--- index.js
+++ index.js
@@ -20,3 +20,62 @@ function findByName(restaurant){
   }
 });
 }
+function findByZip(zipcode){
+  Restaurant.findOne({}, function(err, docs ){
+    docs.forEach(function(rest){
+        if(rest.address.zipcode === zipcode){
+          results.push(rest);
+        }
+      });
+      console.log(results);
+    });
+  }
+
+function update(restaurant, update){
+  Restaurant.findOneAndUpdate({name: restaurant}, {name: update}, {new: true}, function(err, docs){
+    iff(err){
+      console.log(err)
+    }else{
+      console.log(docs);
+    }
+  });
+}
+
+function destroy(resturant){
+  Restaurant.findOneAndRemove({name: resturant}, function(err, docs){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(docs + " was deleted!");
+    }
+  });
+}
+
+function addItem(resturant, item){
+  Restaurant.findOne({name: resturant}, function(err, docs){
+    docs.items.push(new MenuItem({title: item}))
+    docs.save(function(err, results){
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log(results);
+      }
+    })
+  });
+}
+
+function removeItem(resturant, item){
+  Restaurant.findOneAndUpdate({name: resturant}, {
+    $pull: { items: {title: item} }
+  },
+  {new: true}, function(err, docs){
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log(docs);
+    }
+  });
+}
