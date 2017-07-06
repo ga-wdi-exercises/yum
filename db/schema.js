00e95ef8cbdcefcbb2e79dac2e2f6a5a486e9b67
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-07-06 08:52:46 -0400

    part 1

--- db/schema.js
+++ db/schema.js
@@ -2,3 +2,32 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yum');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+  console.log("database has been connected!");
+});
+
+var Schema = mongoose.Schema,
+ ObjectId = Schema.ObjectId
+
+var ItemSchema = new Schema({
+  title: String
+})
+
+var RestSchema = new Schema({
+  name: String,
+  address: {
+    street: String,
+    zipcode: Number
+  },
+  yelpUrl: String,
+  items: [ItemSchema]
+})
+
+var ItemModel = mongoose.model("Item", ItemSchema);
+var RestModel = mongoose.model("Rest", RestSchema);
+
+module.exports ={
+  ItemModel: ItemModel,
+  RestModel: RestModel
+};
